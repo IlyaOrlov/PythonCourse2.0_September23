b1a867f87004d689ed498a2a66bd1facb926de47
Alexander Fedotov <fedotov89@yandex.ru>
2023-08-30 13:55:54 +0300

    Выполнена домашняя работа № 2

---
+++ Practice/AFedotov/Practice2/homework2-2.py
@@ -0,0 +1,6 @@
+start = input("Топлива было: ")
+end = input("Топлива осталось: ")
+distance = input("Расстояние: ")
+diff = int(start) - int(end)
+result = int(diff) / int(distance)
+print(f"Расход бензина: {result}")
