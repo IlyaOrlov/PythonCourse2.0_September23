7bf03c23894ebe4b96c7ea2be2a7d3e523aeeb04
mtroshin <troshin.mike@gmail.com>
2023-08-31 17:24:45 +0300

    Домашнее задание 2. Исправления замечаний.

--- Practice/mtroshin/Lesson_2.py
+++ Practice/mtroshin/Lesson_2.py
@@ -1,8 +1,10 @@
 import math
-radius = input("Введите радиус: ")
+
 
 def square(r):
     return math.pi * r ** 2
 
+
+radius = input("Введите радиус: ")
 result = square(int(radius))
 print(f"Площадь круга: {result}")
--- Practice/mtroshin/lesson_2.2.py
+++ Practice/mtroshin/lesson_2.2.py
@@ -3,7 +3,7 @@ end = input("Топлива осталось: ")
 distance = input("Расстояние: ")
 
 diff = int(start) - int(end)
-
 result = diff / int(distance)
 
-print("Средний расход бензина:", result, "(литров на 1 км)")
+# на всякий случай исправил:)
+print(f"Средний расход бензина: {result} (литров на 1 км)")
