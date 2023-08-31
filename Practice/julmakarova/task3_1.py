cd5c1b9809dd220dd7c2127cacacdbb9a9a663ce
julmakarova <germanovich.julia@mail.ru>
2023-08-31 21:34:07 +0300

    Homework lecture 3 (task 3_1, task 3_2, task 3_3_1, task 3_3_2)

---
+++ Practice/julmakarova/task3_1.py
@@ -0,0 +1,4 @@
+lenght = int(input("Введите длину прямоугольника: "))
+width = int(input("Введите ширину прямоугольника: "))
+perimeter = (lenght+width)*2
+print(f"Периметр прямоугольника с длиной {lenght} и шириной {width} = {perimeter}")
