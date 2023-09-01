90c81222f9ee3c2a68279c492ebdb67f217ce917
Alexander Marinin <vogelrasse@mail.ru>
2023-09-01 21:14:12 +0300

    :)

--- Practice/amarinin/Modul_2/modul2_lesson2_3.py
+++ Practice/amarinin/Modul_2/modul2_lesson2_3.py
@@ -3,43 +3,41 @@ import os
 import hashlib
 import ast
 import argparse
-from time import *
+from time import *  # такой импорт может привести к конфликту между именами в модуле 'time' и именами в коде
 
 
-class shuffler:  # имя класса не соответствует CamelCase
+class shuffler:  # имя класса 'shuffler' не CamelCase
 
     def __init__(self):
         self.map = {}
 
     def rename(self, dirname, output):
-        mp3s = []  # цвет не хороший, вроде функция должна чего то возвращать
-
-    for root, directories, files in os.walk(dirname):
-        for file in files:
-            if file[-3:] == '.mp3':
-                mp3s.append([root, file])
-    for path, mp3 in mp3s:  # внутри класса перед блоком for должна быть пустая строка
-        hashname = self.generateName() + '.mp3'
-        self.map[hashname] = mp3
-        os.rename(path + '/' + mp3), path + '/' + hashname))  # после mp3 кавычка и последняя кавычка лишние
-        f = open(output, 'r')
-        f.write(str(self.map))
+          mp3s = []  # два лишних пробела
+        for root, directories, files in os.walk(dirname):
+            for file in files:
+                if file[-3:] == '.mp3':
+                    mp3s.append([root, file])
+        for path, mp3 in mp3s:
+            hashname = self.generateName() + '.mp3'
+            self.map[hashname] = mp3
+            os.rename(path + '/' + mp3), path + '/' + hashname))  # в 'mp3)' лишняя скобка и 'hashname))' то же
+          f = open(output, 'r')  # не хватает двух пробелов перед 'f'
+          f.write(str(self.map))  # не хватает двух пробелов перед 'f'
 
     def restore(self, dirname, restore_path):
-        with open(filename, '+') as f:
-            self.map = ast.literal_eval(f.read())
-        mp3s = []
-
-    for root, directories, files in os.walk(dirname):
-        for file in files:
-            if file[-3:] == '.mp3':
-                mp3s.append({root, file})
-    for path, hashname in mp3s:  # внутри класса перед блоком for должна быть пустая строка
-        os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))  # последняя кавычка лишняя
-        os.remove(restore_path)
-
-    def generateName(self, seed=time()):  # имя функции не соответствует snake_case
-        return hashlib.md5(str(seed)).hexdigest()
+          with open(filename, '+') as f:  # два лишних пробела относительно 'def'
+            self.map = ast.literal_eval(f.read())  # не хватает двух пробелов перед 'self.map'
+          mp3s = []  # если относится к циклу 'with', то нет 4 пробела, иначе 2 лишних пробела относительно 'def'
+        for root, directories, files in os.walk(dirname):
+            for file in files:
+               if file[-3:] == '.mp3':  # не хватает пробела  перед 'if'
+                    mp3s.append({root, file})  # один пробел лишний, относительно 'if'
+        for path, hashname in mp3s:
+            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))  # лишняя скобка ')'
+        os.remove(restore_path)  # если относится к циклу 'for', то не хватает 4 пробела
+                
+     def generateName(self, seed=time()):  # имя 'generateName' не snake_case и один лишний пробел
+          return hashlib.md5(str(seed)).hexdigest()  # один лишний пробел
 
 
 def parse_arguments():
@@ -53,16 +51,15 @@ def parse_arguments():
     restore_parser.add_argument('restore_map')
     args = parser.parse_args()
     return args
-
-
+# должна быть вторая пустая строка
 def main():
     args = parse_arguments()
     Shuffler = shuffler()
     if args.subcommand == 'rename':
-        if args.output:
-            Shuffler.rename(args.dirname, 'restore.info')
-        else:
-            Shuffler.rename(args.dirname, args.output)
+          if args.output:  # два лишних пробела относительно 'if'
+                Shuffler.rename(args.dirname, 'restore.info') #  два лишних пробела относительно 'if'
+          else:  #  два лишних пробела относительно 'if'
+                Shuffler.rename(args.dirname, args.output)  # два лишних пробела относительно 'else:'
     elif args.subcommand == 'restore':
         Shuffler.restore(args.dirname, args.restore_map)
     else:
