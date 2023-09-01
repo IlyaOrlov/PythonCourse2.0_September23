f9144c7318e4c98395aa13269bb79e9fdc624f42
Alexander Marinin <vogelrasse@mail.ru>
2023-09-01 19:01:42 +0300

    :)

--- Practice/amarinin/Modul_3/modul3_lesson3_1.py
+++ Practice/amarinin/Modul_3/modul3_lesson3_1.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
     length = float(input("Введите длину  прямоугольника (м) : "))
     width = float(input("Введите ширину прямоугольника (м) : "))
-    area = round(length * width, 2)
-    print(f"Площадь прямоугольника равна  {area}  кв.м.")
+    perimeter = round((length + width) * 2, 2)
+    print(f"Периметр прямоугольника равен  {perimeter}  м.")
