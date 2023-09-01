5d550b71a295ff0a558f802b26cb2ef2b1a0fac2
Alexander Marinin <vogelrasse@mail.ru>
2023-09-01 13:58:21 +0300

    :)

---
+++ Practice/amarinin/Modul_3/modul3_lesson3_3_1.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+
+    password = int(input("Введите цифровой пароль (целое число) : "))
+    key = int(input("Введите секретный код   (целое число) : "))
+    key_parole = password ^ key
+    print(f"\nЗашифрованный цифровой пароль   {key_parole}")
---
+++ Practice/amarinin/Modul_3/modul3_lesson3_3_2.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+
+    key_parole = int(input("Введите зашифрованный цифровой пароль (целое число) : "))
+    key = int(input("Введите секретный код   (целое число)               : "))
+    password = key_parole ^ key
+    print(f"\nЦифровой пароль   {password}")
