3c99d4703fac1afd14f7825d5a4e5d714331e2cf
Alexander Marinin <vogelrasse@mail.ru>
2023-09-02 22:26:13 +0300

    :)

--- Practice/amarinin/Modul_3/modul3_lesson3_5.py
+++ Practice/amarinin/Modul_3/modul3_lesson3_5.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
     word = input("Введите слово  : ").lower()
     revers_word = word[::-1]
-    polindrom = word == revers_word
-    print(f"Ваше слово полиндром?  {polindrom}  !!!")
+    palindrom = word == revers_word
+    print(f"Ваше слово палиндром?  {palindrom}  !!!")
