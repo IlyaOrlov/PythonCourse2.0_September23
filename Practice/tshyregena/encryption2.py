672dcf204f3f3ade4f35c5dab8254754e47b6179
Shyregena <meshenkova@bk.ru>
2023-09-04 15:10:46 +0300

    Исправила. Надеюсь, я правильно поняла, что нужно и куда.

--- Practice/tshyregena/encryption2.py
+++ Practice/tshyregena/encryption2.py
@@ -2,9 +2,9 @@ p = input("Введите пароль: ")
 key = 3478
 res = int(p) ^ key
 print(res)
-res2 = res ^ key
 k = int(input("Введите ключ: "))
 if k == key:
+    res2 = res ^ key
     print(res2)
 else:
     print(f"Неверный ключ")
