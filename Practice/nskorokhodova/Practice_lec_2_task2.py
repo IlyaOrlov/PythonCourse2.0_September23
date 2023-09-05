35c9af50b221cfa62369e3ddb38f0fb157fbcf7e
nsa <nscor@yandex.ru>
2023-09-05 20:41:32 +0300

    Practice_Lec_2

---
+++ Practice/nskorokhodova/Practice_lec_2_task2.py
@@ -0,0 +1,5 @@
+start = int(input("Топлива было: ")); end = int (input("Топлива осталось: "))
+distance = int(input("Расстояние: "))
+diff = int(start) - int(end)
+result = diff / distance
+print(f"Расход бензина: {result}")
