78e5daeeb53c48733485c3b8b802acdc6f42515b
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-09-05 16:52:22 +0300

    переделал

--- Practice/amarkov/mod_2_2_1.py
+++ Practice/amarkov/mod_2_2_1.py
@@ -1,9 +1,13 @@
 import math
 
+
 def square(r):
     return math.pi * r ** 2
 
 
 if
+    x = input("Введите первое число: ")
+    y = input("введите второе число: ")
+
     radius = input("Введите радиус: ")
     print(f"Площадь круга: {result}")
