9abdd51fd37ac5dad53d806b0f6465e3fb1ee923
Tamara <orlov.soft.company@gmail.commara>
2023-09-05 13:57:45 +0300

    Исправления внесены

--- Practice/tgridneva/practice 3.5.py
+++ Practice/tgridneva/practice 3.5.py
@@ -1,5 +1,2 @@
-palindrom = input("Введите слово: ")
-if str(palindrom) == str(palindrom)[::-1]:
-    print("True")
-else:
-    print("False")
+palindrom = input("Введите слово: ").lower().replace(' ', '')
+print(palindrom == palindrom[::-1])
