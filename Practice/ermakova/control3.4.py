b0875c186b5d4e7662c237babf1dd0b8dfef1efb
User <mtv.tan@mail.ru>
2023-09-06 14:42:35 +0300

    Третья лекция. Исправление 4 задания

--- Practice/ermakova/control3.4.py
+++ Practice/ermakova/control3.4.py
@@ -1,8 +1,10 @@
+import re
+
+
 word = input("Введите слово: ")
 # русский и английский алфавит
-if "А" in word or "а" in word or "A" in word or "a" in word:
-    word1 = (word.replace("а", "*").replace("А", "*")
-             .replace("a", "*").replace("A", "*"))
+if re.search("[АаAa]", word):
+    word1 = word.replace("а", "*").replace("А", "*").replace("a", "*").replace("A", "*")
     print(f"Итоговый результат: {word1}")
 else:
     print(f"В данном слове нет буквы А!")
