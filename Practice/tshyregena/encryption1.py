4f952057340c4be3fb3beae25b5f9b51b690152b
Shyregena <meshenkova@bk.ru>
2023-09-06 16:27:56 +0300

    Исправила

--- Practice/tshyregena/encryption1.py
+++ Practice/tshyregena/encryption1.py
@@ -1,4 +1,4 @@
 p = input("Введите пароль: ")
 key = 3478
 res = int(p) ^ key
-
+print(res)
--- Practice/tshyregena/encryption2.py
+++ Practice/tshyregena/encryption2.py
@@ -1,10 +1,4 @@
-p = input("Введите пароль: ")
+p = input("Введите зашифрованный пароль: ")
 key = 3478
 res = int(p) ^ key
 print(res)
-k = int(input("Введите ключ: "))
-if k == key:
-    res2 = res ^ key
-    print(res2)
-else:
-    print(f"Неверный ключ")
