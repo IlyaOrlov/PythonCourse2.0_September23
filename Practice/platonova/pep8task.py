323c6321799c0e63fb6a0e4dff6ab22eda40151d
Юля Платонова <yilya21q@gmail.com>
2023-09-07 01:26:59 +0300

    домашнее задание по 2 уроку

--- Practice/platonova/pep8task.py
+++ Practice/platonova/pep8task.py
@@ -6,38 +6,41 @@ import argparse
 from time import *
 
 
+# Название класса должно соответствовать CamelCase, например Shuffler
 class shuffler:
 
     def __init__(self):
         self.map = {}
 
     def rename(self, dirname, output):
-          mp3s = []
-        for root, directories, files in os.walk(dirname):
-            for file in files:
-                if file[-3:] == '.mp3':
-                    mp3s.append([root, file])
-        for path, mp3 in mp3s:
-            hashname = self.generateName() + '.mp3'
-            self.map[hashname] = mp3
-            os.rename(path + '/' + mp3), path + '/' + hashname))
-          f = open(output, 'r')
-          f.write(str(self.map))
+        mp3s = []
+
+    for root, directories, files in os.walk(dirname):
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append([root, file])
+    for path, mp3 in mp3s:
+        hashname = self.generateName() + '.mp3'
+        self.map[hashname] = mp3
+        os.rename(path + '/' + mp3), path + '/' + hashname))
+        f = open(output, 'r')
+        f.write(str(self.map))
 
     def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:
+        with open(filename, '+') as f:
             self.map = ast.literal_eval(f.read())
-          mp3s = []
-        for root, directories, files in os.walk(dirname):
-            for file in files:
-               if file[-3:] == '.mp3':
-                    mp3s.append({root, file})
-        for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
+        mp3s = []
+
+    for root, directories, files in os.walk(dirname):
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append({root, file})
+    for path, hashname in mp3s:
+        os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
         os.remove(restore_path)
-                
-     def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()
+
+    def generateName(self, seed=time()):
+        return hashlib.md5(str(seed)).hexdigest()
 
 
 def parse_arguments():
@@ -52,14 +55,17 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+
+# Не хватает еще одной строчки (Отделяйте функции верхнего уровня и определения классов двумя пустыми строками)
 def main():
     args = parse_arguments()
+    # Для имен переменных следует выбирать нижний регистр
     Shuffler = shuffler()
     if args.subcommand == 'rename':
-          if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
-          else:
-                Shuffler.rename(args.dirname, args.output)
+        if args.output:
+            Shuffler.rename(args.dirname, 'restore.info')
+        else:
+            Shuffler.rename(args.dirname, args.output)
     elif args.subcommand == 'restore':
         Shuffler.restore(args.dirname, args.restore_map)
     else:
