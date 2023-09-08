b6bfb15566530fcf191352b408a8a013b4afa915
User <mtv.tan@mail.ru>
2023-09-08 08:49:36 +0300

    Четвертая лекция. Первые два задания.

---
+++ Practice/ermakova/control4.2.py
@@ -0,0 +1,8 @@
+while not ((x := input("Введите пятизначное число ")).isdecimal() and len(x)) == 5:
+    pass
+
+i = 0
+while i < len(x):
+    if x[i].isdecimal():
+        print(f"{i+1} цифра равна {x[i]}\n")
+    i += 1
