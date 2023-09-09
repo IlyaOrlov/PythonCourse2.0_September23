bb87e07a6f746ffc83a946691ef01495cb815af7
Екатерина <123@mail.ru>
2023-09-09 18:11:51 +0300

    Удалила инициализацию i

--- Practice/Kashintseva/task4_2.py
+++ Practice/Kashintseva/task4_2.py
@@ -1,8 +1,6 @@
 x = input("Введите пятизначное число: ")
-i = 0
 if x.isdecimal() and len(x) == 5:
     for i in range(len(x)):
         print(f"{i+1} цифра равна {x[i]}")
-        i += 1
 else:
     print("Ошибка! Вы ввели не пятизначное число!")
