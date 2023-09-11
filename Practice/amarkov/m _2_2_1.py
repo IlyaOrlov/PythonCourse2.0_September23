4d78de7e019ad9dacb825ef863c5f9812f705161
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-09-11 12:32:30 +0300

    так должно быть верно

--- Practice/amarkov/m _2_2_1.py
+++ Practice/amarkov/m _2_2_1.py
@@ -4,9 +4,9 @@ import math
 def square(r):
     return math.pi * r ** 2
 
-
-if
-    x = input("введите число пи: ")
+    if condition:
+    x = input("введите число : ")
     radius = input("Введите радиус: ")
+
     result = square(int(radius))
     print(f"Площадь круга: {result}")
