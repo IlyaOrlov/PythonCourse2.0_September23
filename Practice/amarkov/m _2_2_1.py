b042802228be37b9891c559bfe3529b0fba507f1
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-09-11 12:10:18 +0300

    переделал

--- Practice/amarkov/m _2_2_1.py
+++ Practice/amarkov/m _2_2_1.py
@@ -6,8 +6,8 @@ def square(r):
 
 
 if
-    x = input("введите первое число: ")
-    x = input("введите второе число: ")
+    x = input("введите число пи: ")
+    y = input("введите второе число: ")
 
     radius = input("Введите радиус: ")
     result = square(int(radius))
