2a6c00ce8f78dffeb0ecf2032764c283642dd6e5
Tamara <orlov.soft.company@gmail.commara>
2023-09-11 18:28:59 +0300

    вроде бы исправила

--- Practice/tgridneva/practice 4.6.py
+++ Practice/tgridneva/practice 4.6.py
@@ -1,5 +1,19 @@
-x = max(1, 10)
-print(x)
-a = (1, 10)
-y = max(a) 
-print(y)
+def num(a, b):
+    if a > b:
+        _max = a
+        print(a)
+    elif b > a:
+        _max = b
+        print(b)
+
+
+def num2(a, b):
+    if a > b:
+        _max = a
+    elif b > a:
+        _max = b
+        return a
+
+
+if __name__ == '__main__':
+    num(a=2, b=3)
