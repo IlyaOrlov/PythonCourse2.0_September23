bce848c0ca46ae5656f450e1c0d63273d9c7b79f
Tamara <orlov.soft.company@gmail.commara>
2023-09-11 16:15:39 +0300

    исправлено

--- Practice/tgridneva/practice 4.2.py
+++ Practice/tgridneva/practice 4.2.py
@@ -1,6 +1,6 @@
 temp = input('Введите пятизначное число: ')
-assert len(temp), "Число не пятизначное"
+assert (len(temp) == 5)
 result = []
 if temp.isdecimal() and len(temp) == 5:
     for i, n in enumerate(temp, 1):
-        print(f"{temp} == {n}")
+        print(f"{i} == {n}")
