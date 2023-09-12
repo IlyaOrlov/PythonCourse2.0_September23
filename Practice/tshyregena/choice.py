30cc79ca706a1e150d03646c7d45e3f7dc579200
Shyregena <meshenkova@bk.ru>
2023-09-12 16:18:24 +0300

    Исправила

--- Practice/tshyregena/choice.py
+++ Practice/tshyregena/choice.py
@@ -5,15 +5,15 @@ p = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
 num = random.randint(p, b)
 i = input("Отгадайте число: ")
-while not i.isdecimal() == num:
+while i.isdecimal():
     x = int(i)
     if x == num:
         print("Вы угадали!")
+        break
     elif x > num:
         print(f"Ошибка! Загаданное число меньше!")
-        i = input("Попробуйте ещё раз: ")
     else:
         print(f"Ошибка! Загаданное число больше!")
-        i = input("Попробуйте ещё раз: ")
+    i = input("Попробуйте ещё раз: ")
 else:
     print("Вы ввели не число!")
