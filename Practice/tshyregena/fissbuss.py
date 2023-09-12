469f735f04b73bd06d02fd806541b254ad4f212d
Shyregena <meshenkova@bk.ru>
2023-09-12 16:11:41 +0300

    Исправила

--- Practice/tshyregena/fissbuss.py
+++ Practice/tshyregena/fissbuss.py
@@ -2,10 +2,10 @@ start = 1
 end = 100
 while start < end:
     print(start)
-    if start % 3 == 0:
-        print(f"Fizz")
+    if start % 15 == 0:
+        print(f"FizzBuzz")
     elif start % 5 == 0:
         print(f"Buzz")
-    if start % 15 == 0:  # Не поняла почему, если ставить ещё раз elif, не работает
-        print(f"FizzBuzz")
+    elif start % 3 == 0:
+        print(f"Fizz")
     start += 1
