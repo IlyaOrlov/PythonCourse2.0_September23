5607ea4aaf84d4d1d0034e99c156d33ab8a1b78e
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-09-12 14:07:31 +0300

    Всё работает нормально без ошибок. Площадь круга была вычислена.

--- Practice/amarkov/mod2_2_1.py
+++ Practice/amarkov/mod2_2_1.py
@@ -5,7 +5,7 @@ def square(r):
     return math.pi * r ** 2
 
 
-if
+if __name__ == "__main__":
     radius = input("Введите радиус: ")
     result = square(int(radius))
     print(f"Площадь круга: {result}")
