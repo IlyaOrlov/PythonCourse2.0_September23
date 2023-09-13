33aefd06de183724b45cd8774c06b6a8aa80556d
CAndrey <chernovandreey@gmail.com>
2023-09-13 04:29:42 +0300

    модуль: 4 задание: 8 убрал косяк)

--- Practice/achernov/module_4/task_8.py
+++ Practice/achernov/module_4/task_8.py
@@ -1,16 +1,13 @@
 import random
 
 
-def game():
-    pass
-
-
 weapon = ["камень", "ножницы", "бумага"]
 bot_choice = random.choice(weapon)
 my_choice = input("Выбери камень, ножницы или бумагу: ")
 
 while my_choice == bot_choice:
     print(f"Оба игрока выбрали {my_choice}, Ничья!")
+    break
 else:
     if (my_choice == "бумага" and bot_choice == "камень" or
             my_choice == "камень" and bot_choice == "ножницы" or
