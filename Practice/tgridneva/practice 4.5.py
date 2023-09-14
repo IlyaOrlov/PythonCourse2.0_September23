f6b28ff7b7baafd36f6ccf3d391c1a3c1ae2c962
Tamara <orlov.soft.company@gmail.commara>
2023-09-14 17:01:16 +0300

    исправлен

--- Practice/tgridneva/practice 4.5.py
+++ Practice/tgridneva/practice 4.5.py
@@ -2,13 +2,15 @@ from random import randint
 
 number = randint(1, 10)
 quess = 0
-while quess != number:
-    quess = int(input("Угадайте целое число от 1 до 10:"))
+
+while (quess := input("Угадайте целое число от 1 до 10:")).isdecimal():
+    quess = int(quess)
     if quess < number:
         print("Ваше число меньше, чем задумал компьютер")
     elif quess > number:
         print("Ваше число больше, чем задумал компьютер")
     else:
         print("Вы угадали")
+        break
 print(quess)
 print(number)
