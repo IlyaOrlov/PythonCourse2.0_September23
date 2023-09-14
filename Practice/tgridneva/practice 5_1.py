a77ee5ef8cf6bb56d3820b8781ed889d556a19c0
Tamara <orlov.soft.company@gmail.commara>
2023-09-14 13:31:19 +0300

    замечания учтены

--- Practice/tgridneva/practice 5_1.py
+++ Practice/tgridneva/practice 5_1.py
@@ -1,14 +1,13 @@
 def selection_sort(array_to_sort=None):
-    a = array_to_sort
-    for i in range(len(a)):
+    if array_to_sort is None:
+        array_to_sort = ()
+    for i in range(len(array_to_sort)):
         idx_min = i
-        for j in range(i + 1, len(a)):
-            if a[j] < a[idx_min]:
+        for j in range(i + 1, len(array_to_sort)):
+            if array_to_sort[j] < array_to_sort[idx_min]:
                 idx_min = j
-        tmp = a[idx_min]
-        a[idx_min] = a[i]
-        a[i] = tmp
-    return a
+        array_to_sort[idx_min], array_to_sort[i] = array_to_sort[i], array_to_sort[idx_min]
+    return array_to_sort
 
 
 ary = [0, 3, 24, 2, 3, 7]
