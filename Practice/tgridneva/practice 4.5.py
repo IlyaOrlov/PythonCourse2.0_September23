5633ee3ebab84289c426f11bb2bda40e5ad9400a
Tamara <orlov.soft.company@gmail.commara>
2023-09-14 14:45:07 +0300

    вроде бы переделала

--- Practice/tgridneva/practice 4.5.py
+++ Practice/tgridneva/practice 4.5.py
@@ -1,8 +1,9 @@
 from random import randint
 
 number = randint(1, 10)
-quess = int(input("Угадайте целое число от 1 до 10:"))
+quess = 0
 while quess != number:
+    quess = int(input("Угадайте целое число от 1 до 10:"))
     if quess < number:
         print("Ваше число меньше, чем задумал компьютер")
     elif quess > number:
