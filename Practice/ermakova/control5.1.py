83a6edc59da5d0061731d847f7cca7c93ee5878a
User <mtv.tan@mail.ru>
2023-09-14 23:22:53 +0300

    Пятая лекция. 1,  задание, исправление

--- Practice/ermakova/control5.1.py
+++ Practice/ermakova/control5.1.py
@@ -1,6 +1,8 @@
 arr = [0, 3, 24, 2, 3, 7]
-for i in range(len(arr)):
+for i in range(len(arr)-1):
     for n in range(i+1, len(arr)):
+        x_min = i
         if arr[n] < arr[i]:
-            arr[i], arr[n] = arr[n], arr[i]
+            x_min = n
+        arr[i], arr[x_min] = arr[x_min], arr[i]
 print(arr)
