5098e94ed70daa4e264f01a7a33a05dc4bdc9c2b
Mikhail Volobuev <volobuev-m@yandex.ru>
2023-09-14 08:44:35 +0300

    Лекция 5, задания 1,2,3,4. Исправления внесены. arg[i], arg[k] = arg[k], arg[i]

--- Practice/mvolobuev/lec5 task1.py
+++ Practice/mvolobuev/lec5 task1.py
@@ -3,7 +3,5 @@ print(arg)
 for i in range(0, len(arg)-1):
     for k in range(i+1, len(arg)):
         if int(arg[k]) < int(arg[i]):
-            ar = arg[i]
-            arg[i] = arg[k]
-            arg[k] = ar
+            arg[i], arg[k] = arg[k], arg[i]
 print(arg)
