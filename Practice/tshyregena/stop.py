e56a7a8893cfd6c6a53247f1b2ee3a9fe19fc2c2
Shyregena <meshenkova@bk.ru>
2023-09-16 22:32:05 +0300

    Исправила

--- Practice/tshyregena/stop.py
+++ Practice/tshyregena/stop.py
@@ -1,15 +1,9 @@
-p = input("Когда вы захотите остановиться, скажите 'STOP'. Введите число: ")
 s = ""
-p1 = p.lower()
-p2 = "stop"
-while p1 != p2:
-    if p.isalpha():
-        print(f"Вы ввели не число!")
-        p = input("Введите число: ")
+while (p := input("Введите число, когда захотите остановиться введите 'STOP': ")).lower() != "stop":
+    if p.isdecimal():
         s += p
-    elif p1 == p2:
-        break
     else:
-        s += p
-        p = input("Введите следующие число: ")
+        print(f"Вы ввели не число!")
+    print(s)
 print(f"Ваше число: {s}")
+
