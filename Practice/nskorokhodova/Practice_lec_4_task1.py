c189c6d98d32ba650fa1828b7318c8781aab2643
nsa <nscor@yandex.ru>
2023-09-20 16:35:28 +0300

    Д\з по лекции №4 ( задачи 1-5)

--- Practice/nskorokhodova/Practice_lec_4_task1.py
+++ Practice/nskorokhodova/Practice_lec_4_task1.py
@@ -1,10 +1,10 @@
 if __name__ == "__main__":
     for x in range(1, 101):
-        if x % 3 == 0:
+        if x % 15 == 0:
             print("Fizz")
         elif x % 5 == 0:
             print("Buzz")
-        elif x % 15 == 0:
+        elif x % 3 == 0:
             print("FizzBuzz")
         else:
             print(x)
