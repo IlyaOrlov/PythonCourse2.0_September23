db563ad33fda61b9fe528c023bc5c3075a5c8913
Alexander Marinin <vogelrasse@mail.ru>
2023-09-21 19:33:34 +0300

    modul5

--- Practice/amarinin/Modul_5/modul5_lesson5_5.py
+++ Practice/amarinin/Modul_5/modul5_lesson5_5.py
@@ -1,7 +1,6 @@
 def print_file(file):
     with open(file, "r", encoding="utf-8") as f:
-        print(f.read())
-
+        print(repr(f.read()))
 
 def replace_char_file(file, old_char, new_char):
     with open(file, "r", encoding="utf-8") as f:
@@ -14,7 +13,7 @@ if __name__ == "__main__":
     with open("my_f.txt", "w", encoding="utf-8") as new_f:
         new_f.write("1    2    3    4    5")
     print_file("my_f.txt")
-    replace_char_file("my_f.txt", "    ", "\\t")
+    replace_char_file("my_f.txt", "    ", "\t")
     print_file("my_f.txt")
-    replace_char_file("my_f.txt", "\\t", "    ")
+    replace_char_file("my_f.txt", "\t", "    ")
     print_file("my_f.txt")
