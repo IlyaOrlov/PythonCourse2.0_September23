0396ac4c3abc845821c691d463f6a43fbfea3f90
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-09-22 16:59:58 +0300

    сохранил изменения

--- Practice/amarkov/exzample 1.py
+++ Practice/amarkov/exzample 1.py
@@ -1,7 +1,8 @@
-x= 3
-y = 2
-urt = 3 - 2
-print(urt)
+word = "арзамас"
+print(len(word)) # узнал сколько в слове символов
 
-decod = urt + y
+for el in range(len(word)): # перебрал слово в цикле
+    print(el,[0,7])
 
+for el in range(len(word)): # обратиться по индексу к каждой букве слова
+    print(word[el])
