7879af7f5d9601b9c3560b209951f4c1b7ba3968
User <mtv.tan@mail.ru>
2023-09-23 12:58:14 +0300

    Пятая лекция. 4 задание.Изменения

--- Practice/ermakova/control5.4.py
+++ Practice/ermakova/control5.4.py
@@ -2,16 +2,13 @@ lst = [[1, 2, 5, 7],
        [3, 2, 2, 1],
        [8, 5, 0, 2]]
 number = int(input("Введите число, столбцы с которым надо удалить: "))
-ind = []
-for i in range(0, len(lst)):
-    for j in range(0, len(lst[i])):
-        if lst[i][j] == number:
-            if j in ind:
-                j += 1
-            else:
-                ind.append(j)
-ind.sort(reverse=True)
-for i in range(0, len(lst)):
-    for x in ind:
-        del lst[i][x]
-    print(lst[i], sep="\n")
+ind = set()
+for lst1 in lst:
+    for j in range(0, len(lst1)):
+        if lst1[j] == number:
+            ind.add(j)
+ind_l = sorted(ind, reverse=True)
+for lst1 in lst:
+    for x in ind_l:
+        del lst1[x]
+    print(lst1, sep="\n")
