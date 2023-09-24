f8ebcf6ea3238adfea097d16d0f3e0c4bfb50dc4
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-09-24 17:21:15 +0300

    сохранил изменения

---
+++ Practice/amarkov/modul3_4.py
@@ -0,0 +1,13 @@
+word = "арзамас"
+print(len(word)) # узнал сколько в слове символов
+
+for el in range(len(word)): # перебрал слово в цикле
+    print(el,[0,7])
+
+for el in range(len(word)): # обратиться по индексу к каждой букве слова
+    print(word[el])
+
+for word in range(7):
+    print(word)
+    word = "а"
+    print(word)
