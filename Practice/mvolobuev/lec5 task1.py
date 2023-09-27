87ad4231dece584c93ab5d659afd3eaf495f73a2
Mikhail Volobuev <volobuev-m@yandex.ru>
2023-09-27 08:41:53 +0300

    Лекция 5, задания 1. Исправления внесены.

--- Practice/mvolobuev/lec5 task1.py
+++ Practice/mvolobuev/lec5 task1.py
@@ -2,9 +2,8 @@ arg = [0, 3, 24, 2, 3, 7]
 print(arg)
 for i in range(0, len(arg)-1):
     n = i
-    for k in range(i+1, len(arg)):
-        if arg[k] < arg[i]:
-            if arg[k] < arg[n]:
-                n = k
+    for k in range(i + 1, len(arg)):
+        if arg[k] < arg[n]:
+            n = k
     arg[i], arg[n] = arg[n], arg[i]
 print(arg)
