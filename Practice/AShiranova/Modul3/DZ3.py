f56e6cebeea0bf34116c58ce865f2c9f5353a60f
Анюта <arara34@bk.ru>
2023-09-27 18:49:02 +0300

    дз 3 лекция

--- Practice/AShiranova/Modul3/DZ3.py
+++ Practice/AShiranova/Modul3/DZ3.py
@@ -1,5 +1,4 @@
-if __name__ == "__main__":
-    password = int(input("Введите цифровой пароль (целое число) : "))
-    key = int(input("Введите секретный код   (целое число) : "))
-    key_parole = password ^ key
-    print(f"\nЗашифрованный цифровой пароль   {key_parole}")
+ParolAnn = int(input("Введите цифровой пароль: "))
+key = 777
+encr_password = ParolAnn ^ key
+print(f"Зашифрованный пароль пользователя (необходимо запомнить) : {encr_password}")
