dca4405715a84fc3d6f37f4ef21dda99775cb034
Анюта <arara34@bk.ru>
2023-09-28 13:03:36 +0300

    поправила

--- Practice/AShiranova/Modul4/DZ1.py
+++ Practice/AShiranova/Modul4/DZ1.py
@@ -1,7 +1,10 @@
 start = 1
 end = 101
 for i in range(start, end):
-    if i % 15 == 0: print("FizzBuzz")
-    elif i % 3 == 0: print("Fizz")
-    elif i % 5 == 0: print("Buzz")
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
     else: print(i)
