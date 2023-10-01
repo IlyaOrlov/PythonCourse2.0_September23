09a01c534ba00d6ed9df4c3d3e8db0f57ac01031
nsa <nscor@yandex.ru>
2023-10-02 02:16:59 +0300

    Д\з по лекции №4 ( задачи 6-8)

---
+++ Practice/nskorokhodova/Practice_lec_5_task5.py
@@ -0,0 +1,17 @@
+def change(file_path, x):
+    with open(file_path, 'r') as f:
+        content = f.read()
+    if x == "развернуть":
+        content = content.replace('\t', '    ')
+    elif x == "свернуть":
+        content = content.replace('    ', '\t')
+    else:
+        print("Некорректный ввод")
+
+    with open(file_path, 'w') as f:
+        f.write(content)
+
+
+my_file = input("Введите путь к файлу: ")
+x = input("Выберите развернуть или свернуть символы табуляции: ")
+change(my_file, x)
