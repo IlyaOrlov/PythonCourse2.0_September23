71665cac2e598ee1bd0574a216be08ab7eff9f93
nsa <nscor@yandex.ru>
2023-10-02 02:25:52 +0300

    Д\з по лекции №5

--- Practice/nskorokhodova/Practice_lec_5_task1.py
+++ Practice/nskorokhodova/Practice_lec_5_task1.py
@@ -1,7 +1,7 @@
 arr = [0, 3, 24, 2, 3, 7]
 for i in range(len(arr)):
     min_i = i
-    for x in range(i+1, len(arr)):
+    for x in range(i + 1, len(arr)):
         if arr[x] < arr[min_i]:
             min_i = x
     if min_i != i:
