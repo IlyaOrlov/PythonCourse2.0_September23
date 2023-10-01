774a2ca26750cea63c6584fdd1a7b7f0ce804a1d
Юля Платонова <yilya21q@gmail.com>
2023-10-01 05:43:15 +0300

    Задание 6 к лекции 4

---
+++ Lectures/lec_4_6.py
@@ -0,0 +1,8 @@
+a = int(input("первое число: "))
+b = int(input("второе число: "))
+def max_of_two(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+print(max_of_two(a, b))
---
+++ Lectures/lec_4_6_2.py
@@ -0,0 +1,7 @@
+a = int(input("первое число: "))
+b = int(input("второе число: "))
+def max_of_two(a, b):
+    if a > b:
+        return a
+    else:
+        return b
