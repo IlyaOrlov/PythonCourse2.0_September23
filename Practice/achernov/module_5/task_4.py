a9bd44a03cf7c62c458a1f75e44a70bbbdcd96c2
CAndrey <chernovandreey@gmail.com>
2023-10-02 15:24:03 +0300

    модуль: 5 задание 4 поправил

--- Practice/achernov/module_5/task_4.py
+++ Practice/achernov/module_5/task_4.py
@@ -1,14 +1,17 @@
 def remove(matrix, digit):
     for line in matrix:
-        if digit in line:
-            index = line.index(digit)
-            for i in range(len(matrix)):
-                del matrix[i][index]
+        i = 0
+        while i != len(line):
+            if line[i] == digit:
+                for lst in matrix:
+                    lst.pop(i)
+            else:
+                i += 1
     return matrix
 
 
-my_matrix = [[1, 2, 3],
+my_matrix = [[1, 2, 2],
              [4, 5, 6],
              [7, 8, 9]]
-new_matrix = remove(my_matrix, 2)
-print(my_matrix)
+new_matrix = remove(my_matrix, int(input("Введите число: ")))
+print(new_matrix)
