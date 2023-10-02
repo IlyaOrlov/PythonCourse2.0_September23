3ad11bfd9fd968f5959e232c8e0cc4b9b0656b93
Юля Платонова <yilya21q@gmail.com>
2023-10-02 06:26:07 +0300

    исправления к заданию 4.6 по лекции 4

--- Practice/platonova/lec_4_6_2.py
+++ Practice/platonova/lec_4_6_2.py
@@ -1,7 +1,10 @@
-a = int(input("первое число: "))
-b = int(input("второе число: "))
 def max_of_two(a, b):
     if a > b:
         return a
     else:
         return b
+
+
+a = int(input("первое число: "))
+b = int(input("второе число: "))
+max_of_two(a, b)
