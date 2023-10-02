10686e45eda10d7f78406b3ca06c0b85b941a986
Mikhail Volobuev <volobuev-m@yandex.ru>
2023-10-02 08:56:13 +0300

    Лекция 5, Поправил.

--- Practice/mvolobuev/lec5 task5_2.py
+++ Practice/mvolobuev/lec5 task5_2.py
@@ -1,13 +1,11 @@
 # Замена проберов на Tab
 s1 = ""
-s2 = ""
+s2 = "\n"
 slist = ""
 with open("Piton.txt", "r", encoding="utf-8") as f1:
     for s in f1:
         s1 = " " + s.strip()
-        s3 = s1.replace(" ", "\t", 1)
-        slist = slist + s2 + s3
-        s2= "\n"
+        slist += s1.replace(" ", "\t") + s2
         print(slist)
 
 with open("Piton.txt","w",encoding= "utf-8") as f:
