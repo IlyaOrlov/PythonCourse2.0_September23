dc7165db026a9221160fdeabeface89dadf04896
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-10-04 13:05:31 +0300

    как перевернуть и сравнить слово

--- Practice/amarkov/exzample 1.py
+++
@@ -1,9 +0,0 @@
-word = "арзамас"
-print(len(word)) # узнал сколько в слове символов
-
-for el in range(len(word)): # перебрал слово в цикле
-    print(el,[0,7])
-
-for el in range(len(word)): # обратиться по индексу к каждой букве слова
-    print(word[el])
-    print(word.find("а"))
