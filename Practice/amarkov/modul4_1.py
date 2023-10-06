2106725f08d3cd8729b3e200d68c888319a7366d
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-10-06 13:06:43 +0300

    должно быть правильно.

--- Practice/amarkov/modul4_1.py
+++ Practice/amarkov/modul4_1.py
@@ -1,4 +1,4 @@
-for i in range(1, 101, 15):
+for i in range(1, 101, 5):
     print(i)
 if i % 3:
     print("Fizz")
@@ -8,3 +8,5 @@ else:
     else:
         if i % 15:
             print("FizzBuzz")
+        else:
+            print("i")
