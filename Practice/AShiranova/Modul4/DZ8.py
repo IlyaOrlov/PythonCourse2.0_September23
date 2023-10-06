f94cb019c71cd78a83a06bbfeadf8045f9e78e27
Анюта <arara34@bk.ru>
2023-10-06 11:36:41 +0300

    исправила 8 задание

--- Practice/AShiranova/Modul4/DZ8.py
+++ Practice/AShiranova/Modul4/DZ8.py
@@ -1,24 +1,29 @@
 import random
 while True:
-    a = round(random.uniform(1,3))
+    a = round(random.randint(1,3))
     print ("1 - камень","2 - ножницы","3 - бумага", sep='\n')
-    p = int(input())
-    res = ("камень", "ножницы", "бумага")
-    print(f"Компьютер выбрал {res[a - 1]}")
-    if a == p:
-        print("Ничья")
-    if a == 1 and p==2:
-        print("Вы проиграли")
-    if a == 1 and p==3:
-        print("Вы выйграли!")
-    if a == 2 and p==1:
-        print("Вы выйграли!")
-    if a == 2 and p==3:
-        print("Вы проиграли")
-    if a == 3 and p==1:
-        print("Вы проиграли")
-    if a == 3 and p==2:
-        print("Вы выйграли!")
+    p = input("Введите число от 1 до 3: ")
+
+    if p not in ["1", "2", "3"]:
+        print("Ошибка!Нужно ввести число от 1 до 3")
+    else:
+        p = int(p)
+        res = ("камень", "ножницы", "бумага")
+        print(f"Компьютер выбрал {res[a - 1]}")
+        if a == p:
+            print("Ничья")
+        elif a == 1 and p==2:
+            print("Вы проиграли")
+        elif a == 1 and p==3:
+            print("Вы выйграли!")
+        elif a == 2 and p==1:
+            print("Вы выйграли!")
+        elif a == 2 and p==3:
+            print("Вы проиграли")
+        elif a == 3 and p==1:
+            print("Вы проиграли")
+        elif a == 3 and p==2:
+            print("Вы выйграли!")
     qw = input("Начать заново? (да\нет) ")
     if qw == 'нет':
         break
