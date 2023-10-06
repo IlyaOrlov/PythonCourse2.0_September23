92a0ee170eec362c4b8fb96dc9aeed0684f22f3e
Юля Платонова <yilya21q@gmail.com>
2023-10-06 12:55:42 +0300

    исправления к заданию 5.1 по лекции 5

--- Practice/platonova/lec_5_1.py
+++ Practice/platonova/lec_5_1.py
@@ -1,10 +1,11 @@
 def sort(arr):
     for i in range(len(arr)):
         minimum = i
-        for j in range(i + 1, len(arr)):
+        for j in range(i+1, len(arr)):
             if arr[j] < arr[minimum]:
                 minimum = j
-        arr[minimum], arr[i] = arr[i], arr[minimum]
+        if minimum != i:
+            arr[minimum], arr[i] = arr[i], arr[minimum]
     return arr
 
 
