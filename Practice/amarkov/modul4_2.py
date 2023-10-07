068f732b30869b3b98edf7871458e06dbb887479
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-10-07 17:06:04 +0300

    задача

--- Practice/amarkov/modul4_2.py
+++ Practice/amarkov/modul4_2.py
@@ -1,9 +1,8 @@
 a = input("Введите число: ")
 numbers = []
 
-for char in a:
-     print(char)
-
 i = 1 # ввели переменную от которой начать счёт
+
 for char in a:
      print(f"{i} цифра равна {char}")
+     i += 1
