34043d1194fa9b6d335f5f764fe412d9c2318c08
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-10-07 14:00:22 +0300

    сохранить

--- Practice/amarkov/modul4_2.py
+++ Practice/amarkov/modul4_2.py
@@ -2,6 +2,8 @@ a = input("Введите число: ")
 numbers = []
 
 for char in a:
-    if char.isdigit():
-        print(char)
+     print(char)
 
+i = 1
+for char in a:
+     print(f"{i} цифра равна {char}")
