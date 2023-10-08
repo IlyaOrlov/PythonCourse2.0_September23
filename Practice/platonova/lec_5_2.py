68d8e26334853ec6485aff4adacbfde4be0f263c
Юля Платонова <yilya21q@gmail.com>
2023-10-08 16:35:22 +0300

    исправления к заданию 5.2 по лекции 5

--- Practice/platonova/lec_5_2.py
+++ Practice/platonova/lec_5_2.py
@@ -1,8 +1,10 @@
 def fun_find_dup(lst):
-    for i in range(len(lst) - 1):
-        for j in range(i + 1, len(lst)):
-            if lst[j] == lst[i]:
-                return lst[j]
+    s = set()
+    for i in range(len(lst)):
+        if lst[i] in s:
+            return lst[i]
+        else:
+            s.add(lst[i])
 
 
 print(fun_find_dup([1, 2, 4, 3, 2, 1]))
