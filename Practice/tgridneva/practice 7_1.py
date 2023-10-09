77e9c26f6b9ecea0a78595a2cc4210c71ee16336
Tamara <orlov.soft.company@gmail.commara>
2023-10-09 20:25:26 +0300

    дз 7 лекция 1 задание

---
+++ Practice/tgridneva/practice 7_1.py
@@ -0,0 +1,11 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def solve_task():
+        print("I'm not ready yet")
+
+
+person = Man("John")
+Man.solve_task()
