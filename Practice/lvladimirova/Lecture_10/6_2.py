353f94efb559dbdec895116aeb25db8b8c8acda3
Liliya <zaharova.liliya@mail.ru>
2023-10-09 12:17:41 +0300

    Домашняя работа

---
+++ Practice/lvladimirova/Lecture_10/6_2.py
@@ -0,0 +1,70 @@
+class Duck:
+    color = "серый"
+
+    def __init__(self, name, weight):
+        print("\nПривет! Новая уточка с вами!")
+        self.name = name
+        self.weight = weight
+
+    @staticmethod
+    def say_crack():
+        print("Кряяяяяя!")
+
+    def info_duck(self):
+        print(f"Меня зовут: {self.name}, мой вес: {self.weight} кг")
+
+    def __repr__(self):
+        return f"У нас появилась уточка {self.name}, вес уточки {self.weight} кг"
+
+    @classmethod
+    def duck_color(cls):
+        print(f"Цвет уточки: {cls.color}")
+
+    def __lt__(self, other):
+        return self.weight > other.weight
+
+    def __gt__(self, other):
+        return self.weight < other.weight
+
+    def __eq__(self, other):
+        return self.weight == other.weight
+
+    def __ne__(self, other):
+        return self.weight == other.weight
+
+    def __add__(self, other):
+        new_weight = self.weight + other.weight
+        super_duck = Duck(None, new_weight)
+        print(f"Вес двух уточек: {self.weight + other.weight} кг.")
+        return super_duck
+
+
+duck1 = Duck("Дональд", 7)
+duck1.say_crack()
+duck1.info_duck()
+duck1.duck_color()
+print(repr(duck1))
+
+duck2 = Duck("Билли", 5)
+duck2.say_crack()
+duck2.info_duck()
+duck2.duck_color()
+print(repr(duck2))
+
+duck3 = Duck("Вилли", 9)
+duck3.say_crack()
+duck3.info_duck()
+duck3.duck_color()
+print(repr(duck3))
+
+print("===============================")
+print(duck1 > duck2)
+print("===============================")
+print(duck2 < duck3)
+print("===============================")
+print(duck2 == duck3)
+print("===============================")
+print(duck2 != duck3)
+print("===============================")
+mega_duck = duck1 + duck3
+print(mega_duck.weight)
