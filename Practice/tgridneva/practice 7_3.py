1cc78dfa6e75db15a1e0bc8117ed7dd7374fd944
Tamara <orlov.soft.company@gmail.commara>
2023-10-10 10:59:25 +0300

    исправлено

--- Practice/tgridneva/practice 7_3.py
+++ Practice/tgridneva/practice 7_3.py
@@ -1,16 +1,16 @@
 class ATM:
-    def __init__(self, cash):
-        self.cash = cash
+    def __init__(self, balance):
+        self._balance = balance
 
     def get_cash(self, amount):
-        if amount <= self.cash:
-            self.cash -= amount
+        if amount <= self._balance:
+            self._balance -= amount
             print(f"Withdrew {amount} cash")
         else:
-            print("Insufficient cash")
+            print("Insufficient balance")
 
     def deposit_cash(self, amount):
-        self.cash += amount
+        self._balance += amount
         print(f"Deposited {amount} cash")
 
     def get_supported_operations(self):
@@ -18,15 +18,15 @@ class ATM:
 
 
 class OnlineATM(ATM):
-    def __init__(self, cash):
-        super().__init__(cash)
+    def __init__(self, balance):
+        super().__init__(balance)
 
     def make_payment(self, amount):
-        if amount <= self.cash:
-            self.cash -= amount
+        if amount <= self._balance:
+            self._balance -= amount
             print(f"Made a payment of {amount}")
         else:
-            print("Insufficient cash")
+            print("Insufficient balance")
 
     def get_supported_operations(self):
         return super().get_supported_operations() + ["Make Payment"]
@@ -38,6 +38,15 @@ atm2 = OnlineATM(2000)
 atms = [atm1, atm2]
 
 for atm in atms:
-    print(f"ATM Cash: {atm.cash}")
+    print(f"ATM Balance: {atm._balance}")
     print(f"Supported Operations: {atm.get_supported_operations()}")
+    print("------------------------")
+
+    atm.get_cash(500)
+    atm.deposit_cash(1000)
+
+    if isinstance(atm, OnlineATM):
+        atm.make_payment(300)
+
+    print(f"ATM Balance: {atm._balance}")
     print("------------------------")
