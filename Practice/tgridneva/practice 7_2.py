79f78e74ba1a8d2941db44a1fc882b3c49748cd3
Tamara <orlov.soft.company@gmail.commara>
2023-10-10 11:00:11 +0300

    исправления внесенф

--- Practice/tgridneva/practice 7_2.py
+++ Practice/tgridneva/practice 7_2.py
@@ -2,10 +2,12 @@ import time
 from random import randint
 
 
-class Pupil:
+class Man:
     def __init__(self, name):
         self.name = name
 
+
+class Pupil(Man):
     def solve_task(self):
         sleep_time = randint(3, 6)
         time.sleep(sleep_time)
