6a91095cd6cd9dacfe2bf922f255eecdd80a232d
Alexander Marinin <vogelrasse@mail.ru>
2023-10-10 23:25:27 +0300

    modul7

--- Practice/amarinin/Modul_7/modul7_lesson7_2.py
+++ Practice/amarinin/Modul_7/modul7_lesson7_2.py
@@ -7,20 +7,18 @@ class Man:
     def __init__(self, name):
         self.name = name
 
-    @staticmethod
-    def solve_task():
+    def solve_task(self):
         print("I`m not ready yet")
 
 
 class Pupil(Man):
 
-    @staticmethod
-    def solve_task():
-        print("0", end="")
+    def solve_task(self):
+        print("0", end="->")
         for i in range(randint(3, 6)):
             sleep(1)
-            print(f"_{i + 1}", end="")
-        print("\nI`m not ready yet")
+            print(f"{i + 1}", end="->")
+        super().solve_task()
 
 
 if __name__ == "__main__":
