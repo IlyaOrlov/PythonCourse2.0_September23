8a3d705df439bc5880cafa73378d07a676348bdb
julmakarova <germanovich.julia@mail.ru>
2023-10-11 15:51:06 +0300

    Correction of remarks.

--- Practice/julmakarova/task_7_1_2.py
+++ Practice/julmakarova/task_7_1_2.py
@@ -12,11 +12,9 @@ class Man:
 
 
 class Pupil(Man):
-    @staticmethod
-    def solve_task():
+    def solve_task(self):
         time.sleep(random.randint(3, 6))
-        super(Pupil, Pupil).solve_task() # как я поняла, super() со статическим методом работает только в такой интерпретации
-                                         # спасибо stackoverflow:) ну либо прописывать явно Man.solve_task()
+        super().solve_task()
 
 
 m = Man("Mihail")
