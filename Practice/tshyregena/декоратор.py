52286269f2ed3efa9730a0975b0fd5bc2d8afd00
Shyregena <meshenkova@bk.ru>
2023-10-11 22:13:54 +0300

    Поправила, но правильно ли?

--- Practice/tshyregena/декоратор.py
+++ Practice/tshyregena/декоратор.py
@@ -1,17 +1,18 @@
 def fun_dec(fun):
     def fun_nuw(*args):
         print("="*10)
-        print(max(args))
+        res = fun(*args)
+        print(res)
         print("="*10)
 
     return fun_nuw
 
 
 @fun_dec
-def fun(*args):
-    print(max(args))
+def fun1(*args):
+    return max(args)
 
 
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
-fun(a, b)
+fun1(a, b)
