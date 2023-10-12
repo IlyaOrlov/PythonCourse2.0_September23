8055d2fffaa84452499ef2ca6787672900fe45b7
Shyregena <meshenkova@bk.ru>
2023-10-12 23:18:22 +0300

    Исправила

--- Practice/tshyregena/спящий парень.py
+++ Practice/tshyregena/спящий парень.py
@@ -7,20 +7,20 @@ class Man:
         self.name = name
 
     @staticmethod
-    def solve_task(self):
+    def solve_task():
         print(f"I'm not ready yet")
 
 
 class Pupil(Man):
 
     @staticmethod
-    def solve_task(self):
+    def solve_task():
         r = random.randrange(3, 6)
         time.sleep(r)
         print(f"Я подумал {r} секунды, но я тоже не готов пока.")
 
 
 m = Man("Гоша")
-m.solve_task(m)
+m.solve_task()
 h = Pupil("Лев")
-h.solve_task(h)
+h.solve_task()
