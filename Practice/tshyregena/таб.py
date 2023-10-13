1ce55831468ebf118fb3d47e817fb9586e7dc8de
Shyregena <meshenkova@bk.ru>
2023-10-13 22:21:42 +0300

    Так лучше?

--- Practice/tshyregena/таб.py
+++ Practice/tshyregena/таб.py
@@ -1,9 +1,9 @@
-with open("таб.txt", "r", encoding="utf-8") as f:
+with open("таб.txt", "r+", encoding="utf-8") as f:
     s = f.read()
     p = input("Свернуть или развернуть символы табуляции: ").lower()
     k = ""
     if p == "свернуть":
-        k = s.replace('    ', '\t')
+        k = s.replace('\t', ' ')
+        f.write(k)
     else:
         print(s)
-    print(k)
