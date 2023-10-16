35aa0c4e0b0a35a9cdcf994a52c5571af2df02b6
mtroshin <troshin.mike@gmail.com>
2023-10-16 23:37:25 +0300

    Домашнее задание Лекция 5. Правки по 2 и 4 заданию.

--- Practice/mtroshin/Lecture_5/2.py
+++ Practice/mtroshin/Lecture_5/2.py
@@ -1,10 +1,18 @@
+# def povtor(arr):
+#     for i in range(len(arr)):
+#         for j in range(0, i - 1):
+#             if j < len(arr):
+#                 if arr[i] == arr[j]:
+#                     print(arr[i])
+#                     return arr[i]
+
 def povtor(arr):
-    for i in range(len(arr)):
-        for j in range(0, i - 1):
-            if j < len(arr):
-                if arr[i] == arr[j]:
-                    print(arr[i])
-                    return arr[i]
+    mnogestvo = set('')
+    for i in range(len(arr)-1):
+        if arr[i] in mnogestvo:
+            print(arr[i])
+        else:
+            mnogestvo.add(arr[i])
 
 arr = [2, 3, 4, 5, 3, 2]
 povtor(arr)
--- Practice/mtroshin/Lecture_5/4.py
+++ Practice/mtroshin/Lecture_5/4.py
@@ -1,13 +1,23 @@
+# def udalenie(s):
+#     for i in lst1:
+#         for d in i:
+#             if int(s) == d:
+#                 udal = i.index(d)
+#                 for t in lst1:
+#                     t.pop(udal)
+
 def udalenie(s):
+    n = int(s)  # чтоб не преобразовывать в цикле на каждой итерации
     for i in lst1:
-        for d in i:
-            if int(s) == d:
-                udal = i.index(d)
+        j = 0
+        while j < len(i):
+            if n == i[j]:
                 for t in lst1:
-                    t.pop(udal)
+                    t.pop(j)
+            else:
+                j += 1
 
-
-lst1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+lst1 = [[1, 2, 2], [4, 5, 6], [7, 8, 9]]
 print("Изначальная матрица:\n")
 for i in lst1:
     print(i)
