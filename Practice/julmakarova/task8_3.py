9a0457fcbb8742c9cd3a8b22244315c64abf7544
julmakarova <germanovich.julia@mail.ru>
2023-10-18 22:45:38 +0300

    Homework lecture 8(task8_1,task8_2, task8_3, task8_4)

---
+++ Practice/julmakarova/task8_3.py
@@ -0,0 +1,19 @@
+import time
+
+
+class TimeManager:
+    def __init__(self):
+        self._start = 0
+
+    def __enter__(self):
+        self._start = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        stop = time.time()
+        print(f"Время выполнения данного кода: {stop - self._start} c.")
+
+
+with TimeManager():
+    n = int(input("Введите целое число: "))
+    lst = [n ** i for i in range(5)]
+    print(lst)
