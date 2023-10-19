d0af11889ed18b969749261739dddeb7e2166771
Tamara <orlov.soft.company@gmail.commara>
2023-10-19 18:45:11 +0300

    добавила

--- Practice/tgridneva/Практика 8_4.py
+++ Practice/tgridneva/Практика 8_4.py
@@ -1,5 +1,8 @@
+import itertools
+
+
 def merge_arrays(arr1, arr2, arr3):
-    return arr1 + arr2 + arr3
+    return list(itertools.chain(arr1 + arr2 + arr3))
 
 
 input_arrays = ([1, 2, 3], [4, 5], [6, 7])
