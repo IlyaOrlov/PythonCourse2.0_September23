6fd18974600530e899d67685ecc2fb34910643e5
RemoSet <vogelrasse@mail.ru>
2023-10-21 01:17:24 +0300

    modul10

--- Practice/amarinin/Modul_10/modul10_lesson9_2.py
+++ Practice/amarinin/Modul_10/modul10_lesson9_2.py
@@ -5,9 +5,9 @@ def long_work(d1, d2):
     start = d1 if d1 < d2 else d2
     res = 0
     for _ in range(abs(d2 - d1).days):
-        if start.isoweekday() in (1, 2, 3, 4, 5):
+        if start.isoweekday() < 6:
             res += 1
-        start = start + dt.timedelta(days=1)
+        start += dt.timedelta(days=1)
     print(f"Всего дней {abs(d2 - d1).days} из них рабочих {res}")
 
 
