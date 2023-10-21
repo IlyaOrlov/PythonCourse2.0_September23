b8c2197cdeb74af6a80c2e148bbdd831afe24e0a
Tamara <orlov.soft.company@gmail.commara>
2023-10-21 12:10:08 +0300

    переделано

--- Practice/tgridneva/Практика 8_1.py
+++ Practice/tgridneva/Практика 8_1.py
@@ -1,26 +1,29 @@
 class ParagraphReader:
-    def __init__(self, text):
+    def __init__(self, text, symbol):
         self._text = text
-        self._paragraphs = []
+        self._symbol = symbol
+        self._i = 0
 
-    def read_paragraphs(self):
-        paragraph = ''
-        for char in self._text:
-            if char == '\n':
-                if paragraph:
-                    self._paragraphs.append(paragraph)
-                    paragraph = ''
-            else:
-                paragraph += char
-        if paragraph:
-            self._paragraphs.append(paragraph)
-        return self._paragraphs
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._i >= len(self._text):
+            raise StopIteration
+
+        start = self._i
+        while self._i < len(self._text) and self._text[self._i] != self._symbol:
+            self._i += 1
+
+        paragraph = self._text[start:self._i]
+        self._i += 1
+
+        return paragraph
 
 
 text = "This is the first paragraph.\n\nThis is the second paragraph.\n\nThis is the third paragraph."
 
-reader = ParagraphReader(text)
-paragraphs = reader.read_paragraphs()
+reader = ParagraphReader(text, symbol='\n')
 
-for paragraph in paragraphs:
+for paragraph in reader:
     print(paragraph)
