572ec649c53e43cc59cf410219d54cc2719198f9
markoffarteom_1987@mail.ru <markoffarteom_1987@mail.ru>
2023-10-21 16:47:25 +0300

    исправлено.

--- Practice/amarkov/modul4_1.py
+++ Practice/amarkov/modul4_1.py
@@ -1,12 +1,12 @@
 for i in range(1, 101, 15):
     print(i)
-    if i % 15:
+    if i % 15 == 0:
         print("FizzBuzz")
 else:
-    if i % 5:
+    if i % 5 == 0:
         print("Buzz")
     else:
-        if i % 3:
+        if i % 3 == 0:
             print("Fizz")
         else:
             print(i)
