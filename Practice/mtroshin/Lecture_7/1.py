270070e57556e6cdc11658f0a3587be40db01bb1
mtroshin <troshin.mike@gmail.com>
2023-10-21 18:15:57 +0300

    Домашнее задание Лекция 7. ООП. Правки по 1 и 2 задаче + 3 задача

--- Practice/mtroshin/Lecture_7/1.py
+++ Practice/mtroshin/Lecture_7/1.py
@@ -1,13 +1,26 @@
-class tank:
+class Tank:
     id = ""
     model = "Т-72"
-    massa = 46
-    pushka_mm = 125
-    dvigatel = 1130
-    boekomplekt = 41
+    weight = 46
+    gun_mm = 125
+    point = 0
 
     def __init__(self, id):
         self.id = id
 
-t = tank(1)
-t2 = tank(2)
+    def go(self, point):
+        self.point = self.point + point
+        print(f"Танк {self.id} проехал {point} метров")
+    def shoot (self):
+        print(f"Танк {self.id} сделал выстрел!")
+
+t1 = Tank(1)
+t2 = Tank(2)
+
+t1.go(20)
+t1.shoot()
+t2.go(50)
+t1.go(10)
+t2.shoot()
+t2.go(33)
+print(f"Координаты танков: {t1.point=} и {t2.point=}")
--- Practice/mtroshin/Lecture_7/2.py
+++ Practice/mtroshin/Lecture_7/2.py
@@ -2,16 +2,16 @@ class Duck:
     color = 'yellow'
 
     def __init__(self, name, weight):
-        self.name = name
-        self.weight = weight
+        self._name = name
+        self._weight = weight
 
     @staticmethod
     def say():
         return 'Crack'
 
     @classmethod
-    def colors(self):
-        return self.color
+    def colors(cls):
+        return cls.color
 
     def name(self):
         return self.name
@@ -23,18 +23,19 @@ class Duck:
         return f'<Duck name={self.name()} weight={self.weight()} color={self.color}>'
 
     def __lt__(self, other):
-        return self.weight() < other.weight()
+        return self.weight < other.weight
 
     def __gt__(self, other):
-        return self.weight() > other.weight()
+        return self.weight > other.weight
 
     def __eq__(self, other):
-        return self.weight() == other.weight()
+        return self.weight == other.weight
 
     def __ne__(self, other):
-        return self.weight() != other.weight()
+        return self.weight != other.weight
 
     def __add__(self, other):
-        newduck = Duck
-        newduck.weight = self.weight() + other.weight()
+        newduck = Duck(f"{self.name + other.name}", 0)
+        newduck.weight = self.weight + other.weight
+
         return newduck
