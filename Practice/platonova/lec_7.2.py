7a9c0e30b53f09acf95f6fe3a8e039211222a6d3
Юля Платонова <yilya21q@gmail.com>
2023-10-23 12:28:33 +0300

    Корректировка 2 задания по лекции 7

--- Practice/platonova/lec_7.2.py
+++ Practice/platonova/lec_7.2.py
@@ -6,6 +6,7 @@ class Man:
     def __init__(self, name):
         self.name = name
 
+    @staticmethod
     def solve_task():
         print("I'm not ready yet")
 
@@ -14,7 +15,7 @@ class Pupil(Man):
     def solve_task(self):
         thinking_time = random.randint(3, 6)
         time.sleep(thinking_time)
-        print("I'm not ready yet")
+        super(Pupil, Pupil).solve_task()
 
 
 x = Pupil("Иван")
