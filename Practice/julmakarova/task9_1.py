e68d2103afd3e320a1ce9df968f58f50e27d48c7
julmakarova <germanovich.julia@mail.ru>
2023-10-25 13:51:11 +0300

    Correction of remarks.

--- Practice/julmakarova/task9_1.py
+++ Practice/julmakarova/task9_1.py
@@ -1,10 +1,16 @@
 import re
 
-with open(r"..\README.md", encoding="utf-8") as f:
-    all_text = f.read()
 
-lst = re.findall(r"(git c.{5,7} [^\"]*)", all_text)
-lst1 = re.findall(r"(git \w{3,5} [^()\"1]*)", all_text)
-lst += lst1
+lst = []
+
+with open(r"..\README.md", encoding="utf-8") as f:
+    for line in f:
+        l1 = re.findall(r"(git c.{5,7} [^\"]*)", line)
+        if l1:
+            lst += l1
+        l2 = re.findall(r"(git \w{3,5} [^()\"1]*)", line)
+        if l2:
+            lst += l2
+
 for i in lst:
     print(i)
--- Practice/julmakarova/task9_2.py
+++ Practice/julmakarova/task9_2.py
@@ -1,17 +1,31 @@
 import datetime as dt
 
 
-def add_long_holiday(holiday, duration):
-    for i in range(0, duration):
-        n_hol = holiday + dt.timedelta(days=i)
-        holidays.append(n_hol)
+class Holidays:
+    HOLIDAYS = frozenset([dt.date(2023, 2, 23), dt.date(2023, 3, 8),
+                          dt.date(2023, 6, 12), dt.date(2023, 11, 4),
+                          dt.date(2023, 12, 31)])
+
+    def __init__(self):
+        self._set_hol = Holidays.HOLIDAYS
+
+    @property
+    def set_hol(self):
+        return self._set_hol
+
+    def add_holiday(self, h_day, duration):
+        if isinstance(self._set_hol, frozenset):
+            self._set_hol = set(self._set_hol)
+        for i in range(0, duration):
+            n_hol = h_day + dt.timedelta(days=i)
+            self._set_hol.add(n_hol)
 
 
-def work_days(start_date, stop_date):
+def work_days(start_date, stop_date, holidays):
     w_days = 0
     i_date = start_date
     while i_date != stop_date:
-        if i_date.weekday() in range(0, 5) and not(i_date in holidays):
+        if i_date.weekday() < 5 and i_date not in holidays:
             w_days += 1
 
         i_date += dt.timedelta(days=1)
@@ -19,15 +33,12 @@ def work_days(start_date, stop_date):
     return w_days
 
 
-holidays = [dt.date(2023, 2, 23), dt.date(2023, 3, 8),
-            dt.date(2023, 6, 12), dt.date(2023, 11, 4),
-            dt.date(2023, 12, 31)]
-
-add_long_holiday(dt.date(2023, 1, 1), 8)
-add_long_holiday(dt.date(2023, 5, 1), 9)
-
+holiday = Holidays()
+holiday.add_holiday(dt.date(2023, 1, 1), 8)
+holiday.add_holiday(dt.date(2023, 5, 1), 9)
 
 d1 = dt.date(2023, 1, 1)
 d2 = dt.date(2023, 1, 13)
 
-print(f"Количество рабочих дней между заданными датами: {work_days(d1, d2)}")
+print(f"Количество рабочих дней между заданными датами: {work_days(d1, d2, holiday.set_hol)}")
+
