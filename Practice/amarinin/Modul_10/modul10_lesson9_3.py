4502173162af9c4c0a26c7c3367a5fb30404e27d
RemoSet <vogelrasse@mail.ru>
2023-10-25 23:59:12 +0300

    modul10

--- Practice/amarinin/Modul_10/modul10_lesson9_3.py
+++ Practice/amarinin/Modul_10/modul10_lesson9_3.py
@@ -1,21 +1,23 @@
 import os
 import time
 import shutil
+from pathlib import Path
 
 
 def close_time_dir_file(path):
     for i in os.listdir(path):
-        if os.path.isfile(path + '\\' + i):
-            if round(time.time() - os.path.getctime(path + '\\' + i)) > 60:
-                print("Файл ", path + '\\' + i, "удален")
-                os.remove(path + '\\' + i)
-        else:
-            if len(os.listdir(path + '\\' + i)) == 0 and (round(time.time() - os.path.getctime(path + '\\' + i)) > 120):
-                print(f"Папка ", path + '\\' + i, "удалена")
-                shutil.rmtree(path + '\\' + i)
-    for i in os.listdir(path):
-        if os.path.isdir(path + '\\' + i):
-            close_time_dir_file(path + '\\' + i)
+        # cur_path = os.path.join(path, i)
+        cur_path = Path(path, i)
+        if os.path.isfile(cur_path):
+            if round(time.time() - os.path.getctime(cur_path)) > 60:
+                print("Файл ", cur_path, "удален")
+                os.remove(cur_path)
+        if os.path.isdir(cur_path):
+            if len(os.listdir(cur_path)) == 0 and (round(time.time() - os.path.getctime(cur_path)) > 120):
+                print(f"Папка ", cur_path, "удалена")
+                shutil.rmtree(cur_path)
+            else:
+                close_time_dir_file(cur_path)
 
 
 if __name__ == "__main__":
@@ -27,5 +29,6 @@ if __name__ == "__main__":
         print("Её нужно заполнить директориями и файлами, секунд за 30-ть :)")
         print("Иначе они протухнут!")
     else:
+        print("Погнали!")
         while os.listdir(dir_name):
             close_time_dir_file(dir_name)
