e5f279a087483a922375a84092f2f2e8b5073a87
Liliya <zaharova.liliya@mail.ru>
2023-10-31 15:49:24 +0300

    Исправление

--- Practice/lvladimirova/Exam2/task5.py
+++ Practice/lvladimirova/Exam2/task5.py
@@ -1,26 +1,33 @@
 import os
-import shutil
 
 
 def copy_file(source, destination):
     try:
-        if os.path.isfile(source):
-            with open(source, 'r') as f_src:
-                data = f_src.read()
-            with open(destination, 'x') as f_dest:
-                f_dest.write(data)
-        elif os.path.isdir(source):
-            shutil.copytree(source, destination)
-        else:
-            raise ValueError("Неверный путь")
-    except (FileNotFoundError, FileExistsError, ValueError) as e:
-        print(e)
+        with open(source, 'r') as f_src:
+            data = f_src.read()
+        with open(destination, 'x') as f_dest:
+            f_dest.write(data)
+    except FileNotFoundError as e:
+        print("Ошибка: файл", e.filename, "не найден.")
+    except FileExistsError as e:
+        print("Ошибка: файл", e.filename, "уже существует.")
 
 
-source_file = "file1.txt"
-destination_file_valid = "file2.txt"
-copy_file(source_file, destination_file_valid)
+def copydir(source_dir, destination_dir):
+    try:
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+        for filename in os.listdir(source_dir):
+            source_file = os.path.join(source_dir, filename)
+            destination_file = os.path.join(destination_dir, filename)
+            if os.path.isfile(source_file):
+                copy_file(source_file, destination_file)
+            else:
+                os.makedirs(source_file)
+    except FileNotFoundError as e:
+        print("Ошибка: директория", e.filename, "не найдена.")
 
-source_dir = "dir1"
-destination_dir = "dir2"
-copy_file(source_dir, destination_dir)
+
+s_dir = "/path/to/source_dir"
+d_dir = "/path/to/destination_dir"
+copydir(s_dir, d_dir)
