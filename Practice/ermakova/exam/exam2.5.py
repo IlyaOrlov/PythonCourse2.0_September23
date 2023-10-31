17ab89c18354889199677a2b2f5b6b73260d190f
User <mtv.tan@mail.ru>
2023-10-31 22:52:32 +0300

    Исправление 2 экзамен 5 задание.

--- Practice/ermakova/exam/exam2.5.py
+++ Practice/ermakova/exam/exam2.5.py
@@ -19,14 +19,12 @@ def copydir(source_dir, destination_dir):
         if not os.path.exists(destination_dir):
             os.makedirs(destination_dir)
             for super_dirs, dirs, files in os.walk(source_dir):
+                d2 = os.path.join(destination_dir, os.path.relpath(super_dirs, source_dir))
+                os.makedirs(d2)
                 for file in files:
                     s = os.path.join(super_dirs, file)
                     des = os.path.join(destination_dir, os.path.relpath(s, source_dir))
                     copyfile(s, des)
-                for name in dirs:
-                    s1 = os.path.join(super_dirs, name)
-                    d2 = os.path.join(destination_dir, os.path.relpath(s1, source_dir))
-                    copydir(s1, d2)
     except FileNotFoundError:
         print(f"Директория {destination_dir} не существует")
 
