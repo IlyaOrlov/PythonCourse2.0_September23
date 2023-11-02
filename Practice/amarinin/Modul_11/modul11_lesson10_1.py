5c8583353281b0d0f164b954acd76b3e0635aef0
RemoSet <vogelrasse@mail.ru>
2023-11-03 00:08:32 +0300

    modul11

--- Practice/amarinin/Modul_11/modul11_lesson10_1.py
+++ Practice/amarinin/Modul_11/modul11_lesson10_1.py
@@ -5,44 +5,53 @@ import time
 
 def find_primes_step_1(start, end):
     lst_prime = []
-    tim_start = time.time()
+    t_start = time.time()
     for num in range(start, end + 1):
-        for i in range(2, num, 1):
+        for i in range(2, int(num ** 0.5), 1):
             if num % i == 0:
                 break
-            if i == num - 1:
-                lst_prime.append(num)
-    print(tim := round(time.time() - tim_start, 2), end="   ")
-    return [lst_prime, tim]
+        else:
+            lst_prime.append(num)
+    tim = round(time.time() - t_start, 3)
+    str(tim).ljust(5, "0")
+    print(str(tim).ljust(5, "0"), end="   ")
 
 
 if __name__ == "__main__":
-    tim_start = time.time()
-    find_primes_step_1(3, 10000)
-    find_primes_step_1(10001, 20000)
-    find_primes_step_1(20001, 30000)
-    print(f"общее время {round(time.time() - tim_start, 2)}")
+    data = [(3, 100000), (100001, 200000), (200001, 300000)]
 
+    res1 = []
     tim_start = time.time()
-    thread1 = threading.Thread(target=find_primes_step_1, args=(3, 10000))
-    thread1.start()
-    thread2 = threading.Thread(target=find_primes_step_1, args=(10001, 20000))
-    thread2.start()
-    thread3 = threading.Thread(target=find_primes_step_1, args=(20001, 30000))
-    thread3.start()
-    thread1.join()
-    thread2.join()
-    thread3.join()
-    print(f"общее время {round(time.time() - tim_start, 2)}")
+    for dt in data:
+        find_primes_step_1(dt[0], dt[1])
+    tim_total = str(round(time.time() - tim_start, 3)).ljust(5, '0')
+    print(f"общее время {tim_total} ")
 
+    res2 = []
     tim_start = time.time()
-    mult1 = multiprocessing.Process(target=find_primes_step_1, args=(3, 10000))
-    mult1.start()
-    mult2 = multiprocessing.Process(target=find_primes_step_1, args=(10001, 20000))
-    mult2.start()
-    mult3 = multiprocessing.Process(target=find_primes_step_1, args=(20001, 30000))
-    mult3.start()
-    mult1.join()
-    mult2.join()
-    mult3.join()
-    print(f"общее время {round(time.time() - tim_start, 2)}")
+    for dt in data:
+        th = threading.Thread(target=find_primes_step_1, args=(dt[0], dt[1]))
+        th.start()
+        res2.append(th)
+    for th in res2:
+        th.join()
+    tim_total = str(round(time.time() - tim_start, 3)).ljust(5, '0')
+    print(f"общее время {tim_total} ")
+
+    res3 = []
+    tim_start = time.time()
+    for dt in data:
+        mult = multiprocessing.Process(target=find_primes_step_1, args=(dt[0], dt[1]))
+        mult.start()
+        res3.append(mult)
+    for mult in res3:
+        mult.join()
+    tim_total = str(round(time.time() - tim_start, 3)).ljust(5, '0')
+    print(f"общее время {tim_total} ")
+
+#  Если не выполнить функцию start(), но выполнить join(), будет выброшена ошибка
+#  AssertionError: can only join a started process
+#  Eсли запустить функцию start(), но не выполнить join(), интерпретатор не будет ждать
+#  завершение обозначенного потока или процесса и при возможности продолжит
+#  выполнение основного потока или просесса. И это может привести к наружению логики
+#  и непредсказуемости результата. появляется ассинхронность.
