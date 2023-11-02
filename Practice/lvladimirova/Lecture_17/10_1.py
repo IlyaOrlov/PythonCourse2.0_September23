f9c96e6e2fc7949c876c2a745148ad975ab9db51
Liliya <zaharova.liliya@mail.ru>
2023-11-02 12:49:24 +0300

    Исправление

--- Practice/lvladimirova/Lecture_17/10_1.py
+++ Practice/lvladimirova/Lecture_17/10_1.py
@@ -27,18 +27,22 @@ if __name__ == "__main__":
     primes3 = find_primes(20001, 30000)
     end_time = time.time()
     execution_time = end_time - start_time
-    print(f"Последовательное выполнение: {execution_time} сек")
+    print(f"Последовательное выполнение: {execution_time} сек.")
 
     print("==========================")
 
     start_time = time.time()
-    t1 = threading.Thread(target=find_primes, args=(3, 10000))
-    t2 = threading.Thread(target=find_primes, args=(10001, 20000))
-    t3 = threading.Thread(target=find_primes, args=(20001, 30000))
-    threads = [t1, t2, t3]
-    for t in threads:
-        t.start()
-        t.join()
+
+    threads = []
+    for x in range(3, 30001, 10000):
+        y = x + 9999
+        thr = threading.Thread(target=find_primes, args=(x, y))
+        thr.start()
+        threads.append(thr)
+
+    for thr in threads:
+        thr.join()
+
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Параллельное выполнение потоков: {execution_time} сек.")
@@ -46,13 +50,17 @@ if __name__ == "__main__":
     print("==========================")
 
     start_time = time.time()
-    p1 = multiprocessing.Process(target=find_primes, args=(3, 10000))
-    p2 = multiprocessing.Process(target=find_primes, args=(10001, 20000))
-    p3 = multiprocessing.Process(target=find_primes, args=(20001, 30000))
-    process = [p1, p2, p3]
-    for p in process:
-        p.start()
-        p.join()
+
+    process = []
+    for x in range(3, 30001, 10000):
+        y = x + 9999
+        prc = multiprocessing.Process(target=find_primes, args=(x, y))
+        prc.start()
+        process.append(prc)
+
+    for prc in process:
+        prc.join()
+
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Параллельное выполнение процессов: {execution_time} сек.")
