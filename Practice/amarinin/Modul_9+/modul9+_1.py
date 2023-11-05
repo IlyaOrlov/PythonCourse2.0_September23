de695c802d5c65e1cfbcfbeac10b44d8cbdfef7f
RemoSet <vogelrasse@mail.ru>
2023-11-05 23:26:21 +0300

    modul9+

---
+++ Practice/amarinin/Modul_9+/modul9+_1.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        lst = ['10', '5', 'a', '3', 'b']
+        print(new_lst1 := [int(x) * int(x) for x in [i for i in lst if i.isdecimal()] if int(x) % 5 == 0])
+        # :(  ни как не могу понять, куда вставить :=
