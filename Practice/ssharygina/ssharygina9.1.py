5f941d0a191faf8ca7708416d8f9bd46b7486f7a
Svetlana Sharygina <svmok18@mail.ru>
2023-11-06 14:03:57 +0300

    Практическое задание 1 из лекции 9 (исправления)

--- Practice/ssharygina/ssharygina9.1.py
+++ Practice/ssharygina/ssharygina9.1.py
@@ -1,11 +1,13 @@
 import re
 
 
-def g_com(file):
-    with open(file, "r", encoding="utf-8") as f:
-        return f.read()
-
+lst = []
+with open(r"..\README.md", "r", encoding="utf-8") as f:
+    x = re.compile(r'git\s[a-z]*\s-{1,2}\w+\s\w*\.*\w*', re.I)
+    for line in f:
+        line = line.rstrip('\n')
+        lst += re.findall(x, line)
 
 if __name__ == "__main__":
-    line = g_com(r"C:\Users\user\PycharmProjects\PythonCourse2.0_September23\Practice\README.md")
-    print(list(set(re.findall(r'git\s[a-z]*\s-{1,2}\w+\s\w*\.*\w*', line))))
+    for i in lst:
+        print(i)
