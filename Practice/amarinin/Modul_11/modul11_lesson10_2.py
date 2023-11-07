07475ca9e3c2d07519d117f9f758cfb81129c68e
RemoSet <vogelrasse@mail.ru>
2023-11-07 21:49:42 +0300

    modul9+

--- Practice/amarinin/Modul_11/modul11_lesson10_2.py
+++ Practice/amarinin/Modul_11/modul11_lesson10_2.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool
 
 def add_all(d):
     time.sleep(0.1)
-    print(f"N:{os.getpid() : < 8}{d[0] + d[1]}") if str(type(d[0])) == str(type(d[1])) \
+    print(f"N:{os.getpid() : < 8}{d[0] + d[1]}") if (type(d[0])) == (type(d[1])) \
         else print(os.getpid(), None)
 
 
--- Practice/amarinin/Modul_9+/modul9+_1.py
+++ Practice/amarinin/Modul_9+/modul9+_1.py
@@ -1,5 +1,3 @@
 if __name__ == "__main__":
-    if __name__ == "__main__":
-        lst = ['10', '5', 'a', '3', 'b']
-        print(new_lst1 := [int(x) * int(x) for x in [i for i in lst if i.isdecimal()] if int(x) % 5 == 0])
-        # :(  ни как не могу понять, куда вставить :=
+    lst = ['10', '5', 'a', '3', 'b']
+    print([x * x for i in lst if i.isdecimal() and ((x := int(i)) % 5 == 0)])
