0335547a0b4aae8ae757f4c270ffa76ada3c065b
RemoSet <vogelrasse@mail.ru>
2023-11-10 00:20:33 +0300

    modul12_lesson11_3

--- Practice/amarinin/Modul_12/modul12_lesson11_3.py
+++ Practice/amarinin/Modul_12/modul12_lesson11_3.py
@@ -6,9 +6,8 @@ if __name__ == "__main__":
     req = request.Request('http://google.com')
     response = request.urlopen(req)
     web_page = response.read().decode()
-    supp = (x for i in web_page.split() if len(x := re.findall(r'href="http.?://.*"', i)))
-    supp_norm_list = [i[0].replace("href=", '').replace("\"", "") for i in supp]
-    for i in supp_norm_list:
+    supp = (re.findall(r'href="(h.*?)"', web_page))
+    for i in supp:
         try:
             a = request.Request(i)
             b = request.urlopen(a)
