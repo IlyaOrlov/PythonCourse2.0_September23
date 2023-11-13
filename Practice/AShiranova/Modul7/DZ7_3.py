e2dec8242d81b1db27f18086078833f5881fe589
Анюта <arara34@bk.ru>
2023-11-14 01:07:20 +0300

    ДЗ модуль 7

---
+++ Practice/AShiranova/Modul7/DZ7_3.py
@@ -0,0 +1,33 @@
+import os
+import tempfile
+
+
+class WrapStrToFile:
+    def __init__(self):
+        self._filepath = tempfile.mktemp()
+
+    @property
+    def content(self):
+        try:
+            with open(self._filepath, "r", encoding="utf-8") as file1:
+                return file1.read()
+        except FileNotFoundError:
+            return 'File doesnt exist'
+
+    @content.setter
+    def content(self, value):
+        with open(self._filepath, "w", encoding="utf-8") as file1:
+            file1.write(value)
+
+    @content.deleter
+    def content(self):
+        os.remove(self._filepath)
+
+
+wstf = WrapStrToFile()
+print(wstf.content)  # Output: File doesn't exist
+wstf.content = 'test str'
+print(wstf.content)  # Output: test_str
+wstf.content = 'text 2'
+print(wstf.content)  # Output: text 2
+del wstf.content     # после этого файла не существует
