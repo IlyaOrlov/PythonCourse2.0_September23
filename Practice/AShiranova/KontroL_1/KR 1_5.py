6c4db5c11016bad408078ce81728e9598fc85093
Анюта <arara34@bk.ru>
2023-11-13 17:53:18 +0300

    Контрольная поправила 5

--- Practice/AShiranova/KontroL_1/KR 1_5.py
+++ Practice/AShiranova/KontroL_1/KR 1_5.py
@@ -1,13 +1,14 @@
-def func_name(test_st):
+def func_name(test_st, symbol):
     my_count = 0
+    symbol_lower = symbol.lower()
     for i in test_st:
-        if i.lower() == symbol.lower():
+        if i.lower() == symbol_lower:
             my_count = my_count + 1
-    print("Ответ : "
-          + str(my_count))
+    return my_count
 
 
 if __name__ == "__main__":
     value = input("Введите строку:")
-    symbol = input("букву:")
-    func_name(value)
+    input_symbol = input("букву:")
+    result = func_name(value, input_symbol)
+    print(f"Ответ {result}")
