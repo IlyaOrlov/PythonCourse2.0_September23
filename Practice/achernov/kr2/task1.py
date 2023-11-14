6ca157b8642aae7b02dd49521be4e7fa0a16f2a3
CAndrey <chernovandreey@gmail.com>
2023-11-15 02:13:56 +0300

    кр2 исправления

--- Practice/achernov/kr2/task1.py
+++ Practice/achernov/kr2/task1.py
@@ -4,17 +4,23 @@ class User:
         self._name = name
         self._age = age
 
-    def set_name(self):
-        pass
+    @property
+    def name(self):
+        print("геттер")
+        return self._name
 
-    def get_name(self):
-        pass
+    @name.setter
+    def name(self, new_name):
+        print("сеттер")
+        self._name = new_name
 
-    def set_age(self):
-        pass
+    @property
+    def age(self):
+        return self._age
 
-    def get_age(self):
-        pass
+    @age.setter
+    def age(self, new_age):
+        self._age = new_age
 
 
 class Worker(User):
@@ -23,18 +29,16 @@ class Worker(User):
         super().__init__(name, age)
         self._salary = salary
 
-    def get_salary(self):
-        pass
-
-    def set_salary(self):
-        pass
-
     @property
     def salary(self):
         return self._salary
+
+    @salary.setter
+    def salary(self, new_salary):
+        self._salary = new_salary
 
 
 w1 = Worker("John", 25, 1000)
 w2 = Worker("Jack", 26, 2000)
 res = w1.salary + w2.salary
-print(res)
+print(f"Суммарная зп = {res} баксов")
--- Practice/achernov/kr2/task3.py
+++ Practice/achernov/kr2/task3.py
@@ -7,10 +7,14 @@ def my_format(string, *args, **kwargs):
         for key, value in kwargs.items():
             string = string.replace("{" + key + "}", str(value))
 
+    for arg in args:
+        string = string.replace("{}", str(arg), 1)
+
     return string
 
 
 hero = "колдун"
 lvl = 99
 print(my_format("Привет, я {0} и я {1} уровня.", hero, lvl))
-print(my_format("Я {hero} и я {age} уровня.", hero=hero, lvl=lvl))
+print(my_format("Я {hero} и я {lvl} уровня.", hero=hero, lvl=lvl))
+print(my_format("Привет, я {} и я {} уровня.", hero, lvl))
