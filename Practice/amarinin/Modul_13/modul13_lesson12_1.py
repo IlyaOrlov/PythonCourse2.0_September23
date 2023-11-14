96963acc29c9cc92aeb251089109b405c5185b9b
RemoSet <vogelrasse@mail.ru>
2023-11-15 01:32:53 +0300

    modul12_lesson11_3

--- Practice/amarinin/Modul_13/modul13_lesson12_1.py
+++ Practice/amarinin/Modul_13/modul13_lesson12_1.py
@@ -1,71 +1,43 @@
 import sqlite3
+import json
 
 
 class MySqlManager:
-
-    def __init__(self, db_name):
-        self.db_name = db_name
+    def __init__(self, dbname):
+        self._connection = sqlite3.connect(dbname)
 
     def __enter__(self):
-        self.connection = sqlite3.connect(self.db_name)
-        self.connection.row_factory = sqlite3.Row
-        self.cursor = self.connection.cursor()
-        print(f"Подключились к базе данных {self.db_name}\n")
+        return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._connection.close()
 
-        self.cursor.close()
-        self.connection.close()
-        print(f"\nБаза данных {self.db_name} закрыта")
+    def configure(self):
+        print(f"CREATE TABLE IF NOT EXISTS User")
+        cursor = self._connection.cursor()
+        schema = ("CREATE TABLE IF NOT EXISTS User"
+                  "    (Id          INTEGER    PRIMARY KEY  AUTOINCREMENT,"
+                  "     First_name  CHAR(128)  NOT NULL,"
+                  "     Last_name   CHAR(64)   NOT NULL,"
+                  "     Age         CHAR(4)    DEFAULT '0')")
+        cursor.execute(schema)
+        self._connection.commit()
 
-    def create_table(self):
-        print("Создаем таблицу")
-        name_table = "CREATE TABLE " + input("Имя таблицы : ").lower().strip()
-        name_prima = input("Имя поля первичного ключа : ").lower().strip() + " INTEGER PRIMARY KEY AUTOINCREMENT"
-        name_fild1 = input("Имена полей через пробел : ").lower().strip().split()
-        name_fild2 = [name_prima] + [i + " " + input(f"Тип поля {i}: ").upper().strip() for i in name_fild1]
-        name_fild2 = " (" + ", ".join(name_fild2) + ")"
-        schema = name_table + name_fild2
-        self.cursor.execute(schema)
-        self.connection.commit()
-        print(f"{schema}\nТаблица создана\n")
+    def insert(self):
+        print(f"INSERT INTO User")
+        cursor = self._connection.cursor()
+        schema = "INSERT INTO User (First_name, Last_name, Age) VALUES ( ?, ?, ? )"
+        cursor.execute(schema,  (input("First_name: "), input("Last_name: "), input("Age: ")))
+        self._connection.commit()
 
-    def insert_data(self):
-        print("Вводим данные")
-        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        table_name = input(f"Выбери таблицу: {[table[0] for table in self.cursor.fetchall()]}  :  ")
-        column_name = self.cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
-        column_names = [col[1] for col in column_name]
-        values_column = [input(f"Введите значение в поле {i}: ") for i in column_names[1:]]
-        schema = f"INSERT INTO {table_name} ({', '.join(column_names[1:])}) VALUES ({', '.join(values_column)})"
-        self.cursor.execute(schema)
-        self.connection.commit()
-        print(f"{schema}\nДанные введены\n")
-
-    def update_data(self):
-        pass
-
-    def delete_data(self):
-        pass
-
-    def select(self):
-        print("Запрос данных")
-        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        table_name = input(f"Выбери таблицу: {[table[0] for table in self.cursor.fetchall()]}  :  ")
-        column_name = self.cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
-        column_names = [col[1] for col in column_name]
-        fields = input(f"Таблица '{table_name}', поля {column_names} Введите * или поля через запятую :  ")
-        schema = f"SELECT {fields} from {table_name}"
-        print(schema)
-        res = self.cursor.execute(schema).fetchall()
-        for s in res:
-            for i in s:
-                print(f"{i} ", end="")
+    def select_all(self):
+        print("SELECT * from User")
+        cursor = self._connection.cursor()
+        print(json.dumps(cursor.execute("SELECT * from User").fetchall()))
 
 
 if __name__ == "__main__":
-    f = MySqlManager("my_data.db")
-    with f:
-        f.create_table()
-        f.insert_data()
-        f.select()
+    with MySqlManager('my_data.db') as db:
+        db.configure()
+        db.insert()
+        db.select_all()
