f3de1603bc98010c99be05d675104e7eafb8d219
Екатерина <123@mail.ru>
2023-11-15 20:09:23 +0300

    Домашнее задание 1 к 12 лекции

---
+++ Practice/Kashintseva/task12_1.py
@@ -0,0 +1,44 @@
+import os
+import sqlite3
+import json
+
+
+class DBC:
+    def __init__(self, d):
+        self._conn = sqlite3.connect(d)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._conn.close()
+
+    def info(self, call, param):
+        cur = self._conn.cursor()
+        cur.row_factory = sqlite3.Row
+        cur.execute(call, param)
+        for row in cur.fetchall():
+            return json.dumps(dict(row))
+
+    def changes(self, call, param):
+        cur = self._conn.cursor()
+        cur.execute(call, param)
+        self._conn.commit()
+        return bool(cur.fetchone())
+
+
+if __name__ == "__main__":
+    data = "childrengr.db"
+    with DBC(data) as db:
+        child_id = int(input("Введите номер ребенка в списке: "))
+        res = db.info("SELECT * FROM Children AS C WHERE C.Id = :child_id", {"child_id": child_id})
+        print(res)
+        new_height = input("Введите новое значение роста ребенка: ")
+        if new_height.isdecimal:
+            new = db.changes("UPDATE Children "
+                             "SET Height = :new_height "
+                             "WHERE Id = :child_id ",
+                             {'сhild_id': child_id, 'new_grup': new_height})
+            print(json.dumps({"Изменение данных": "успешно"}))
+        else:
+            print(json.dumps({"Изменение данных": "ошибка ввода. Данные изменить не удалось"}))
---
+++ Practice/Kashintseva/task12_1_config.py
@@ -0,0 +1,62 @@
+import os
+import sqlite3
+
+
+def configure(conn):
+    cur = conn.cursor()
+
+    cur.execute("CREATE TABLE Children"
+                "    (Id        INTEGER    PRIMARY KEY  AUTOINCREMENT,"
+                "     Name      CHAR(128)  NOT NULL,"
+                "     Age     INTEGER    DEFAULT 0,"
+                "     Height    INTEGER    DEFAULT 0,"
+                "     Weight    INTEGER    DEFAULT 0)")
+
+    cur.execute("CREATE TABLE Gruppa"
+                "   (Id     INTEGER     PRIMARY KEY     AUTOINCREMENT,"
+                "   Name      CHAR(128)     NOT NULL,"
+                "   Floor     INTEGER    DEFAULT 0)")
+
+    cur.execute("CREATE TABLE ChildGrup"
+                "    (ChildId  INTEGER,"
+                "     GruppaId   INTEGER,"
+                "     PRIMARY KEY (ChildId, GruppaId))")
+
+
+def insert_children(conn):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Children (Name, Age, Height, Weight) VALUES ('Popov Misha', 2, 85, 15)")
+    cur.execute("INSERT INTO Children (Name, Age, Height, Weight) VALUES ('Zotova Lera', 3, 100, 18)")
+    cur.execute("INSERT INTO Children (Name, Age, Height, Weight) VALUES ('Kuchina Lena', 2, 86, 14)")
+    cur.execute("INSERT INTO Children (Name, Age, Height, Weight) VALUES ('Noskov Artem', 4, 105, 20)")
+    cur.execute("INSERT INTO Children (Name, Age, Height, Weight) VALUES ('Losina Sasha', 3, 98, 17)")
+    conn.commit()
+
+
+def insert_gruppa(conn):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Gruppa (Name, Floor) VALUES ('Zaiki', 1)")
+    cur.execute("INSERT INTO Gruppa (Name, Floor) VALUES ('Luchiki', 2)")
+    conn.commit()
+
+
+def add_child_to_group(conn, child_id, gruppa_id):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO ChildGrup (ChildId, GruppaId)"
+                " VALUES (:childId, :gruppaId)",
+                {'childId': child_id, 'gruppaId': gruppa_id})
+    conn.commit()
+
+
+if __name__ == "__main__":
+    data = "childrengr.db"
+    if not os.path.exists(data):
+        conn = sqlite3.connect(data)
+        configure(conn)
+        insert_children(conn)
+        insert_gruppa(conn)
+        add_child_to_group(conn, 1, 1)
+        add_child_to_group(conn, 2, 2)
+        add_child_to_group(conn, 3, 1)
+        add_child_to_group(conn, 4, 2)
+        add_child_to_group(conn, 5, 2)
