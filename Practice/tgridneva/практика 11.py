b7da26aee70837babb8a8d0e7459cf727a5ce37b
Tamara <orlov.soft.company@gmail.commara>
2023-11-15 22:40:29 +0300

    практика 11 испра

--- Practice/tgridneva/практика 11.py
+++ Practice/tgridneva/практика 11.py
@@ -13,38 +13,17 @@ def handle_client(client_socket):
 
 
 def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 12345))
-    server_socket.listen(1)
-    print('Server started. Listening on port 12345...')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(('localhost', 12345))
+        server_socket.listen(1)
+        print('Server started. Listening on port 12345...')
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f'Connection established with {client_address[0]}:{client_address[1]}')
-        handle_client(client_socket)
-        client_socket.close()
-
-
-start_server()
-
-import socket
+        while True:
+            client_socket, client_address = server_socket.accept()
+            with client_socket:
+                print(f'Connection established with {client_address[0]}:{client_address[1]}')
+                handle_client(client_socket)
 
 
-def encrypt_word(word):
-    return word
-
-
-def send_encrypted_words(words):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.connect(('localhost', 12345))
-    encrypted_words = [encrypt_word(word) for word in words]
-    request = ','.join(encrypted_words).encode()
-    server_socket.send(request)
-    response = server_socket.recv(1024).decode().split(',')
-    server_socket.close()
-    return response
-
-
-words = ['apple', 'banana', 'cherry']
-decrypted_words = send_encrypted_words(words)
-print('Decrypted words:', decrypted_words)
+if __name__ == '__main__':
+    start_server()
