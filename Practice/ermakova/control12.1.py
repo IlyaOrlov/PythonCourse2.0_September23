afdf476afa7617fe88b2422a4bc1547b69bd105d
User <mtv.tan@mail.ru>
2023-11-15 22:14:23 +0300

    Модуль 12. Первое задание.Исправление 1.

--- Practice/ermakova/control12.1.py
+++ Practice/ermakova/control12.1.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 import json
 
 
@@ -17,20 +18,15 @@ class ClassWrapper:
         cursor = self._conn.cursor()
         try:
             if params:
+                cursor.row_factory = sqlite3.Row
                 cursor.execute(query, *params)
             else:
+                cursor.row_factory = sqlite3.Row
                 cursor.execute(query)
-            rows = cursor.fetchall()
-            columns = [column[0] for column in cursor.description]
-            results = []
-            for row in rows:
-                res = {}
-                for i in range(len(columns)):
-                    res[columns[i]] = row[i]
-                results.append(res)
-            return results
+            results = [dict(row) for row in cursor.fetchall()]
+            return {"STATUS": "SUCCESS", "RESULT": results, "MESSAGE": None}
         except sqlite3.Error as e:
-            return {"STATUS": "ERROR", "MESSAGE": str(e)}
+            return {"STATUS": "ERROR", "RESULT": None, "MESSAGE": str(e)}
 
     def write(self, query, *params):
         try:
@@ -44,16 +40,18 @@ class ClassWrapper:
 
 if __name__ == "__main__":
     db_name = "Car.db"
-    cars = ("CREATE TABLE IF NOT EXISTS Car"
-            "(Id        INTEGER    PRIMARY KEY  AUTOINCREMENT,"
-            " Brand     CHAR(128)  NOT NULL,"
-            " Price     INTEGER    DEFAULT 0,"
-            " Color     CHAR(128)  NOT NULL);")
-
-    with ClassWrapper(db_name) as db:
-        d = db.write(cars)
-        result = db.write("INSERT INTO Car (ID, Brand, Price, Color)"
-                          " VALUES (?, ?, ?, ?)", (1, 'BMW', 10000.00, 'RED'))
-        print(result)
-        data = db.read('SELECT * FROM Car')
-        print(json.dumps(data))
+    if not os.path.exists(db_name):
+        cars = ("CREATE TABLE IF NOT EXISTS Car"
+                "(Id        INTEGER    PRIMARY KEY  AUTOINCREMENT,"
+                " Brand     CHAR(128)  NOT NULL,"
+                " Price     INTEGER    DEFAULT 0,"
+                " Color     CHAR(128)  NOT NULL);")
+        with ClassWrapper(db_name) as db:
+            d = db.write(cars)
+            result = db.write("INSERT INTO Car (ID, Brand, Price, Color)"
+                              " VALUES (?, ?, ?, ?)", (1, 'BMW', 10000.00, 'RED'))
+            print(result)
+    else:
+        with ClassWrapper(db_name) as db:
+            data = db.read('SELECT * FROM Car')
+            print(json.dumps(data))
