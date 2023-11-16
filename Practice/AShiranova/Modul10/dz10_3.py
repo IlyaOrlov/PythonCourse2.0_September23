e07bb236e1b87dde5ef42702e25b342cb632e45e
Анюта <arara34@bk.ru>
2023-11-16 21:07:02 +0300

    дз 10 модуль задание 3

---
+++ Practice/AShiranova/Modul10/dz10_3.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import time
+
+
+def deleting(f):
+    if os.path.isdir(f):
+        while True:
+            x = os.listdir(f)
+            for n in x:
+                pat = os.path.join(f, n)
+                if os.path.isfile(pat):
+                    if time.time() - os.path.getctime(pat) > 60:
+                        os.remove(pat)
+                elif os.path.isdir(pat):
+                    deleting(pat)
+                    if time.time() - os.path.getctime(pat) > 120:
+                        shutil.rmtree(pat)
+    else:
+        print("Вы ввели неккоректный путь!")
+
+
+way = input("Введите путь до папки: ")
+deleting(way)
