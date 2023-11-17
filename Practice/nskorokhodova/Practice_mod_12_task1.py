ea386fa49b8a35de1d136ef31e93ac04c9ce268c
nsa <nscor@yandex.ru>
2023-11-17 18:11:01 +0300

    Д.з. по модулю 12 задача1

--- Practice/nskorokhodova/Practice_mod_12_task1.py
+++ Practice/nskorokhodova/Practice_mod_12_task1.py
@@ -1,4 +1,8 @@
 import math
+import time
+import threading
+import multiprocessing
+
 
 def is_prime(num):
     if num < 2:
@@ -8,6 +12,7 @@ def is_prime(num):
             return False
     return True
 
+
 def find_primes(end, start=3):
     primes = []
     for num in range(start, end + 1):
@@ -15,56 +20,59 @@ def find_primes(end, start=3):
             primes.append(num)
     return primes
 
-import time
 
-# Последовательное выполнение
-start_time = time.time()
-primes1 = find_primes(10000)
-print(f"Время выполнения первого диапазона: {time.time() - start_time} сек")
+if __name__ == '__main__':
 
-start_time = time.time()
-primes2 = find_primes(20000, 10001)
-print(f"Время выполнения второго диапазона: {time.time() - start_time} сек")
+    start_time = time.time()
+    primes1 = find_primes(10000)
+    print(f"Время выполнения первого диапазона: {time.time() - start_time} сек")
 
-start_time = time.time()
-primes3 = find_primes(30000, 20001)
-print(f"Время выполнения третьего диапазона: {time.time() - start_time} сек")
+    start_time = time.time()
+    primes2 = find_primes(20000, 10001)
+    print(f"Время выполнения второго диапазона: {time.time() - start_time} сек")
 
-import threading
+    start_time = time.time()
+    primes3 = find_primes(30000, 20001)
+    print(f"Время выполнения третьего диапазона: {time.time() - start_time} сек")
 
-t1 = threading.Thread(target=find_primes, args=(10000,))
-t2 = threading.Thread(target=find_primes, args=(20000, 10001,))
-t3 = threading.Thread(target=find_primes, args=(30000, 20001,))
+    t1 = threading.Thread(target=find_primes, args=(10000,))
+    t2 = threading.Thread(target=find_primes, args=(20000, 10001,))
+    t3 = threading.Thread(target=find_primes, args=(30000, 20001,))
 
-start_time = time.time()
-t1.start()
-t2.start()
-t3.start()
-print("Потоки запущены")
+    start_time = time.time()
+    t1.start()
+    t2.start()
+    t3.start()
+    print("Потоки запущены")
 
-# Если 'забыть' выполнить start или join для потоков, они так и останутся в состоянии запуска
-# и не приведут к результату. В этом случае программа может зависнуть, так как главный поток
-# не будет дожидаться завершения дочерних потоков и может завершиться раньше их завершения.
+    start = time.perf_counter()
+    threads = []
+    for args in ((10000, 3), (20000, 10001), (30000, 20001)):
+        thr = threading.Thread(target=find_primes, args=args)
+        thr.start()
+        threads.append(thr)
+    for j in threads:
+        j.join()
 
-import multiprocessing
+    print(f'Общее время вычислений потоков в секундах: {time.perf_counter() - start}\n')
 
-# Многопроцессное выполнение
-p1 = multiprocessing.Process(target=find_primes, args=(10000,))
-p2 = multiprocessing.Process(target=find_primes, args=(20000, 10001,))
-p3 = multiprocessing.Process(target=find_primes, args=(30000, 20001,))
+    p1 = multiprocessing.Process(target=find_primes, args=(10000,))
+    p2 = multiprocessing.Process(target=find_primes, args=(20000, 10001,))
+    p3 = multiprocessing.Process(target=find_primes, args=(30000, 20001,))
 
-start_time = time.time()
-p1.start()
-p2.start()
-p3.start()
-print("Процессы запущены")
+    start_time = time.time()
+    p1.start()
+    p2.start()
+    p3.start()
+    print("Процессы запущены")
 
-# Если 'забыть' выполнить start или join для процессов, они так и останутся в состоянии запуска
-# и не приведут к результату. Однако, в отличие от потоков, процессы работают независимо друг от
-# друга, поэтому главный процесс может продолжить выполняться и завершиться, не дожидаясь завершения
-# дочерних процессов. В этом случае дочерние процессы будут продолжать работать независимо до
-# своего завершения.
+    start = time.perf_counter()
+    proc = []
+    for args_proc in ((10000, 3), (20000, 10001), (30000, 20001)):
+        p = multiprocessing.Process(target=find_primes, args=args_proc)
+        p.start()
+        proc.append(p)
+    for pr in proc:
+        pr.join()
 
-start_time = time.time()
-# Выполнение функции или запуск потоков/процессов
-print(f"Время выполнения: {time.time() - start_time} сек")
+    print(f'Общее время вычислений процессов в секундах: {time.perf_counter() - start}\n')
