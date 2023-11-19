550ca18ef5bb86ed46f6e09c426c8f0a9f9a5110
Анюта <arara34@bk.ru>
2023-11-19 19:47:42 +0300

    правка м.10 задание 3

--- Practice/AShiranova/Modul10/dz10_3.py
+++ Practice/AShiranova/Modul10/dz10_3.py
@@ -5,20 +5,21 @@ import time
 
 def deleting(f):
     if os.path.isdir(f):
-        while True:
-            x = os.listdir(f)
-            for n in x:
-                pat = os.path.join(f, n)
-                if os.path.isfile(pat):
-                    if time.time() - os.path.getctime(pat) > 60:
-                        os.remove(pat)
-                elif os.path.isdir(pat):
-                    deleting(pat)
-                    if time.time() - os.path.getctime(pat) > 120:
-                        shutil.rmtree(pat)
+        x = os.listdir(f)
+        for n in x:
+            pat = os.path.join(f, n)
+            if os.path.isfile(pat):
+                if time.time() - os.path.getctime(pat) > 60:
+                    os.remove(pat)
+            elif os.path.isdir(pat):
+                deleting(pat)
+                if time.time() - os.path.getctime(pat) > 120:
+                    shutil.rmtree(pat)
     else:
         print("Вы ввели неккоректный путь!")
 
 
-way = input("Введите путь до папки: ")
-deleting(way)
+if __name__ == "__main__":
+    way = input("Введите путь до папки: ")
+    while True:
+        deleting(way)
