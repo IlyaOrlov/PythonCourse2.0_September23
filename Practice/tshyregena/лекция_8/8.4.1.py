5d8002241f9d4a77ead58e1b168b909188f398b8
Shyregena <meshenkova@bk.ru>
2023-11-21 21:58:20 +0300

    Исправила

--- Practice/tshyregena/лекция_8/8.4.1.py
+++ Practice/tshyregena/лекция_8/8.4.1.py
@@ -1,13 +1,3 @@
-from collections.abc import Iterable
+import itertools
 
-
-def flatten(items: Iterable):
-    for item in items:
-        if isinstance(item, Iterable):
-            yield from flatten(item)
-        else:
-            yield item
-
-
-items1 = ([1, 2, 3], [4, 5], [6, 7])
-print(list(flatten(items1)))
+print(list(itertools.chain([1, 2, 3], [4, 5], [6, 7])))
