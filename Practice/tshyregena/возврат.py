30bbfbb84c864ae1d29ad4d1757e16bf54a2e432
Shyregena <meshenkova@bk.ru>
2023-11-22 23:17:52 +0300

    Исправила

--- Practice/tshyregena/возврат.py
+++ Practice/tshyregena/возврат.py
@@ -1,11 +1,11 @@
 def fun(lst):
-    res = []
+    res = set()
     for element in lst:
         if element in res:
             return element
         else:
-            res.append(element)
+            res.add(element)
 
 
-lst1 = [3, 9, 8, 3, 1, 8, 6]
+lst1 = [4, 9, 8, 9, 1, 8, 6]
 print(fun(lst1))
