622a8dee05a43b5af3a71875341bc77709695354
Shyregena <meshenkova@bk.ru>
2023-11-22 22:25:12 +0300

    Исправила

--- Practice/tshyregena/лекция_8/генератор.py
+++ Practice/tshyregena/лекция_8/генератор.py
@@ -1,20 +1,14 @@
-with open("для_итератора.txt.", "r", encoding="utf-8") as file:
-    def gen():
+
+def gen(name):
+    with open(name, "r", encoding="utf-8") as file:
         for line in file:
             print(file.readline())
             yield line
 
 
-    r = gen()
-    for i in r:
-        print(i)
-    # print(next(r))
-    # print(next(r))
-    # print(next(r))
-
-
-
-
-
-
-
+r = input("Введите имя файла: ")
+for i in gen(r):
+    print(i)
+# print(next(r))
+# print(next(r))
+# print(next(r))
