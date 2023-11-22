cdc9cd424cb83ca0858d26b06ace11d37090c24f
Shyregena <meshenkova@bk.ru>
2023-11-22 23:18:10 +0300

    Пока вроде так

--- Practice/tshyregena/pep8tasksh.py
+++ Practice/tshyregena/pep8tasksh.py
@@ -6,38 +6,38 @@ import argparse
 from time import *
 
 
-class shuffler: # Название класса написано не правильно, как вариант написать с большой буквы
+class Shuffler:
 
-    def __init__(self): # Отсутствует параметр map
+    def __init__(self, map): # Отсутствует параметр map
         self.map = {}
 
     def rename(self, dirname, output): # Может быть статическим методом
-         mp3s = [] # Не выровнено, поэтому не видит
+        mp3s = [] # Не выровнено, поэтому не видит
         for root, directories, files in os.walk(dirname):
             for file in files:
                 if file[-3:] == '.mp3':
                     mp3s.append([root, file])
         for path, mp3 in mp3s:
-            hashname = self.generateName() + '.mp3'
+            hashname = self.generate_name() + '.mp3'
             self.map[hashname] = mp3
             os.rename(path + '/' + mp3), path + '/' + hashname)) # Возможно ошибка в названии mp3
           f = open(output, 'r')
           f.write(str(self.map))
 
-    def restore(self, dirname, restore_path): # Не принимает filename
-          with open(filename, '+') as f: # проблема с выравниванием
+    def restore(self, dirname, restore_path, filename): # Не принимает filename
+        with open(filename, '+') as f: # проблема с выравниванием
             self.map = ast.literal_eval(f.read())
-          mp3s = []
-        for root, directories, files in os.walk(dirname):
-            for file in files:
-               if file[-3:] == '.mp3':
-                    mp3s.append({root, file})
-        for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
-        os.remove(restore_path)
+            mp3s = []
+            for root, directories, files in os.walk(dirname):
+                for file in files:
+                    if file[-3:] == '.mp3':
+                        mp3s.append({root, file})
+            for path, hashname in mp3s:
+                os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
+                os.remove(restore_path)
                 
-     def generateName(self, seed=time()): # Название функции написано с ошибкой
-          return hashlib.md5(str(seed)).hexdigest() # проблема с выравниванием
+    def generate_name(self, seed=time()): # Название функции написано с ошибкой
+        return hashlib.md5(str(seed)).hexdigest() # проблема с выравниванием
 
 
 def parse_arguments():
@@ -54,12 +54,11 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    Shuffler = shuffler() # ошибка в названии класса
     if args.subcommand == 'rename':
-          if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
-          else:
-                Shuffler.rename(args.dirname, args.output)
+        if args.output:
+            Shuffler.rename(args.dirname, 'restore.info')
+        else:
+            Shuffler.rename(args.dirname, args.output)
     elif args.subcommand == 'restore':
         Shuffler.restore(args.dirname, args.restore_map)
     else:
