f6eac33b6d2aea455d0c5f3115776cca18b77ec4
RemoSet <vogelrasse@mail.ru>
2023-11-24 23:52:06 +0300

    Modul14

--- Practice/amarinin/Modul_14/lesson14_1_tests_unittest.py
+++ Practice/amarinin/Modul_14/lesson14_1_tests_unittest.py
@@ -1,5 +1,5 @@
 import unittest
-from lesson14_1_roma import to_roman
+import to_roma
 
 
 class TestUM(unittest.TestCase):
@@ -12,10 +12,14 @@ class TestUM(unittest.TestCase):
     def test_number(self):
         for v, s in {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C',
                      90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V',
-                     4: 'IV', 1: 'I', 0: 'Должно быть 1 - 5000',
-                     5001: 'Должно быть 1 - 5000', 'r': 'Должно быть число'}.items():
-            self.assertTrue(to_roman(str(v)), s)
+                     4: 'IV', 1: 'I'}.items():
+            self.assertTrue(to_roma.to_roman(str(v)), s)
             print(f"test {v} - {s}")
+
+    def test_reises(self):
+        for i in ["hhh", "0", "5001"]:
+            self.assertRaises(x := to_roma.NonValidInput, to_roma.to_roman, i)
+            print("Ok", i,  x.__name__)
 
 
 if __name__ == "__main__":
